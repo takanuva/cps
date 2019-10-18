30c2eff539798869f67c8847d638dab3067fc553
Paulo Torrens <paulotorrens@gnu.org>
2019-10-18 16:42:31 -0300

    Start proofs for substitution

--- theories/SimplyTyped.v
+++ theories/SimplyTyped.v
@@ -12,26 +12,33 @@ Inductive level: Prop :=
   | value
   | command.
 
-(** We use [type] and [prop] as our universes, [void] and [base] represent the
-    bottom and base types respectively. We use de Bruijn indexes on the [bound]
-    constructor. The only type constructor is [negation], a polyadic type which
-    represents the negation of an N-tuple. Our commands are either a [jump],
-    written as [k<x, ...>], or a [bind], written as [b { k<x: t, ...> = c }]. *)
+(** We unify the syntax into the same type in order to make a few proofs easier,
+    though still respecting the above syntactic classes.
+
+    Inspired by the lambda cube, we use [type] and [prop] as our universes, and
+    we keep [base] as our only base type. We also use [void] as the type of
+    commands, though it won't appear on any actual terms. As standard, we use de
+    Bruijn indexes on the [bound] constructor for variables. Types are simple;
+    our only type constructor is [negation], a polyadic type which represents
+    the negation of an N-tuple of types.
+
+    The commands in our language are either a [jump], written as [k<x, ...>], or
+    a [bind], written as [b { k<x: t, ...> = c }]. *)
 
 Inductive pseudoterm: level -> Set :=
   | type:
     pseudoterm sort
   | prop:
     pseudoterm sort
-  | void:
-    pseudoterm sort
   | base:
+    pseudoterm sort
+  | void:
     pseudoterm sort
   | bound:
     forall n: nat,
     pseudoterm value
   | negation:
-    forall xs: list (pseudoterm sort),
+    forall ts: list (pseudoterm sort),
     pseudoterm sort
   | jump:
     forall f: pseudoterm value,
@@ -53,10 +60,10 @@ Definition pseudoterm_deepind:
   forall P: (forall {l: level}, l -> Prop),
   forall f1: P type,
   forall f2: P prop,
-  forall f3: P void,
-  forall f4: P base,
+  forall f3: P base,
+  forall f4: P void,
   forall f5: (forall n: nat, P n),
-  forall f6: (forall xs, List.Forall P xs -> P (negation xs)),
+  forall f6: (forall ts, List.Forall P ts -> P (negation ts)),
   forall f7: (forall f xs, P f -> List.Forall P xs -> P (jump f xs)),
   forall f8: (forall b ts c, P b -> List.Forall P ts -> P c -> P (bind b ts c)),
   forall {l: level} (e: l), P e.
@@ -67,15 +74,15 @@ Proof.
   - apply f1.
   (* Case: prop. *)
   - apply f2.
-  (* Case: void. *)
-  - apply f3.
   (* Case: base. *)
+  - apply f3.
+  (* Case: void. *)
   - apply f4.
   (* Case: bound. *)
   - apply f5.
   (* Case: negation. *)
   - apply f6.
-    induction xs; auto.
+    induction ts; auto.
   (* Case: jump. *)
   - apply f7; auto.
     induction xs; auto.
@@ -92,9 +99,15 @@ Tactic Notation "list" "induction" "over" hyp(H) :=
   [ (* Case: nil. *)
     reflexivity
   | (* Case: cons. *)
-    f_equal; auto].
+    f_equal; auto ].
 
-(** ** Lifting *)
+(** ** Lifting
+
+    As we're dealing with de Bruijn indexes, we need a notion of lifting in our
+    calculus. On the simply typed CPS calculus, most lifing is standard and
+    straightforward, with the notable exception of the binding operation. On a
+    term of the form [b { k<x: t, ...> = c }], we need to bind the continuation
+    [k] on the [b] term, and we need to bind [N] variables on the [c] term. *)
 
 Fixpoint lift (i: nat) (k: nat) {l: level} (e: l): l :=
   match e with
@@ -102,17 +115,17 @@ Fixpoint lift (i: nat) (k: nat) {l: level} (e: l): l :=
     type
   | prop =>
     prop
-  | void =>
-    void
   | base =>
     base
+  | void =>
+    void
   | bound n =>
     if le_gt_dec k n then
       bound (i + n)
     else
       bound n
-  | negation xs =>
-    negation (List.map (lift i k) xs)
+  | negation ts =>
+    negation (List.map (lift i k) ts)
   | jump f xs =>
     jump (lift i k f) (List.map (lift i k) xs)
   | bind b ts c =>
@@ -128,9 +141,9 @@ Proof.
   - reflexivity.
   (* Case: prop.*)
   - reflexivity.
-  (* Case: void.*)
-  - reflexivity.
   (* Case: base.*)
+  - reflexivity.
+  (* Case: void.*)
   - reflexivity.
   (* Case: bound. *)
   - simpl.
@@ -179,9 +192,9 @@ Proof.
   - reflexivity.
   (* Case: prop. *)
   - reflexivity.
-  (* Case: void. *)
-  - reflexivity.
   (* Case: base. *)
+  - reflexivity.
+  (* Case: void. *)
   - reflexivity.
   (* Case: bound. *)
   - simpl.
@@ -223,9 +236,9 @@ Proof.
   - reflexivity.
   (* Case: prop. *)
   - reflexivity.
-  (* Case: void. *)
-  - reflexivity.
   (* Case: base. *)
+  - reflexivity.
+  (* Case: void. *)
   - reflexivity.
   (* Case: bound. *)
   - simpl.
@@ -255,9 +268,9 @@ Proof.
   - reflexivity.
   (* Case: prop. *)
   - reflexivity.
-  (* Case: void. *)
-  - reflexivity.
   (* Case: base. *)
+  - reflexivity.
+  (* Case: void. *)
   - reflexivity.
   (* Case: bound. *)
   - simpl.
@@ -265,7 +278,7 @@ Proof.
     + rewrite lift_bound_ge.
       * rewrite lift_bound_ge; auto with arith.
         do 2 elim plus_assoc_reverse; auto with arith.
-      * eapply le_trans; eauto with arith.
+      * omega.
     + absurd (k <= n); eauto with arith.
     + rewrite lift_bound_ge; auto.
       rewrite lift_bound_lt; auto.
@@ -287,4 +300,110 @@ Proof.
     + do 2 rewrite List.map_length.
       rewrite IHe2; auto with arith.
       replace (i + (l + length ts)) with (i + l + length ts); auto with arith.
+Qed.
+
+(** ** Substitution *)
+
+Fixpoint subst (p: value) (k: nat) {l: level} (q: l): l :=
+  match q with
+  | type =>
+    type
+  | prop =>
+    prop
+  | base =>
+    base
+  | void =>
+    void
+  | bound n =>
+    match lt_eq_lt_dec k n with
+    | inleft (left _) => bound (pred n)
+    | inleft (right _) => lift k 0 p
+    | inright _ => bound n
+    end
+  | negation ts =>
+    negation (List.map (subst p k) ts)
+  | jump f xs =>
+    jump (subst p k f) (List.map (subst p k) xs)
+  | bind b ts c =>
+    bind (subst p (S k) b) (List.map (subst p k) ts) (subst p (k + length ts) c)
+  end.
+
+Lemma subst_bound_gt:
+  forall e k n,
+  n > k -> subst e k n = pred n.
+Proof.
+  intros; simpl.
+  destruct (lt_eq_lt_dec k n) as [ [ ? | ? ] | ? ].
+  - reflexivity.
+  - elim gt_irrefl with k; congruence.
+  - absurd (k <= n); auto with arith.
+Qed.
+
+Lemma subst_bound_eq:
+  forall e k n,
+  n = k -> subst e k n = lift n 0 e.
+Proof.
+  destruct 1; simpl.
+  destruct (lt_eq_lt_dec n n) as [ [ ? | ? ] | ? ].
+  - destruct (gt_irrefl n); auto.
+  - reflexivity.
+  - destruct (lt_irrefl n); auto.
+Qed.
+
+Lemma subst_bound_lt:
+  forall e k n,
+  n < k -> subst e k n = n.
+Proof.
+  intros; simpl.
+  destruct (lt_eq_lt_dec k n) as [ [ ? | ? ] | ? ].
+  - absurd (k <= n); auto with arith.
+  - elim lt_irrefl with k; congruence.
+  - reflexivity.
+Qed.
+
+Lemma lift_addition_distributes_over_subst:
+  forall {l: level} (a: l) b i p k,
+  lift i (p + k) (subst b p a) = subst (lift i k b) p (lift i (S (p + k)) a).
+Proof.
+  induction a using pseudoterm_deepind; intros.
+  (* Case: type. *)
+  - reflexivity.
+  (* Case: prop. *)
+  - reflexivity.
+  (* Case: base. *)
+  - reflexivity.
+  (* Case: void. *)
+  - reflexivity.
+  (* Case: bound. *)
+  - unfold subst at 1.
+    destruct (lt_eq_lt_dec p n) as [ [ ? | ? ] | ? ].
+    + destruct n.
+      inversion l.
+      unfold lift at 1, pred.
+      destruct (le_gt_dec (p + k) n).
+      * rewrite lift_bound_ge; auto with arith.
+        rewrite subst_bound_gt; eauto with arith.
+        replace (i + S n) with (S (i + n)); auto.
+      * rewrite lift_bound_lt; auto with arith.
+        rewrite subst_bound_gt; auto with arith.
+    + destruct e.
+      elim lift_lift_permutation; auto with arith.
+      rewrite lift_bound_lt; auto with arith.
+      rewrite subst_bound_eq; auto with arith.
+    + rewrite lift_bound_lt; auto with arith.
+      rewrite lift_bound_lt; auto with arith.
+      rewrite subst_bound_lt; auto.
+  (* Case: negation. *)
+  - simpl; f_equal.
+    list induction over H.
+  (* Case: jump. *)
+  - simpl; f_equal; auto.
+    list induction over H.
+  (* Case: bind. *)
+  - simpl; f_equal.
+    + replace (S (p + k)) with (S p + k); auto.
+    + list induction over H.
+    + do 2 rewrite List.map_length.
+      replace (p + k + length ts) with ((p + length ts) + k); auto.
+      omega.
 Qed.
