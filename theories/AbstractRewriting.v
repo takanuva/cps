2593158fcbca547e71babe762555e8afb75b249d
Paulo Torrens <paulotorrens@gnu.org>
2021-11-28 19:11:10 -0300

    Add a few stuff about abstract weak bisimilarity

--- theories/AbstractRewriting.v
+++ theories/AbstractRewriting.v
@@ -93,7 +93,7 @@ Section Confluency.
     (* Case: step. *)
     - destruct strip_lemma with z x y as (w, ?, ?); auto.
       exists w; auto with cps.
-    (* Case: tran. *)
+    (* Case: trans. *)
     - rename z0 into w.
       destruct IHclos_trans1 with w as (v, ?, ?); auto.
       destruct IHclos_trans2 with v as (u, ?, ?); auto.
@@ -108,10 +108,10 @@ Section Confluency.
     - exists y; auto with cps.
     (* Case: refl. *)
     - exists x; auto with cps.
-    (* Case: symm. *)
+    (* Case: sym. *)
     - destruct IHclos_refl_sym_trans as (z, ?, ?).
       exists z; auto.
-    (* Case: tran. *)
+    (* Case: trans. *)
     - destruct IHclos_refl_sym_trans1 as (w, ?, ?).
       destruct IHclos_refl_sym_trans2 as (v, ?, ?).
       destruct H with w y v as (u, ?, ?); auto with cps.
@@ -141,7 +141,7 @@ Section Normalization.
     Acc R (f e) -> Acc R e.
   Proof.
     intros.
-    (* There seems to be a bug in the dependent induction tactic... *)
+    (* The dependent induction tactic seems to generalize variables... *)
     remember (f e) as x.
     generalize dependent e.
     induction H0; intros.
@@ -175,9 +175,11 @@ Section ListNormalization.
     SN step_in_list xs.
   Proof.
     induction xs; intros.
+    (* Case: nil. *)
     - constructor; intros.
       exfalso.
       inversion H0.
+    (* Case: cons. *)
     - dependent destruction H.
       apply IHxs in H0; clear IHxs.
       induction H; clear H.
@@ -229,9 +231,8 @@ Section BarbedRelations.
   Definition barbed_bisimulation (S: relation T): Prop :=
     barbed_simulation S /\ barbed_simulation (transp S).
 
-  Definition barbed_bisimilarity: relation T :=
-    fun a b =>
-      exists2 S, barbed_bisimulation S & S a b.
+  Definition barbed_bisimilarity a b: Prop :=
+    exists2 S, barbed_bisimulation S & S a b.
 
   Definition barbed_congruence a b: Prop :=
     forall h,
@@ -250,21 +251,6 @@ Section BarbedRelations.
         destruct H with a b c; firstorder.
       + unfold barb_preserving, transp; intros.
         firstorder.
-  Qed.  
-
-  Lemma barbed_bisimilarity_is_a_bisimulation:
-    barbed_bisimulation barbed_bisimilarity.
-  Proof.
-    apply symmetric_barbed_simulation_is_bisimulation.
-    - split; do 5 intro.
-      + destruct H as (S, ((?, ?), ?), ?).
-        destruct H with a b c as (d, ?, ?); auto.
-        firstorder.
-      + destruct H as (S, ((?, ?), ?), ?).
-        firstorder.
-    - do 3 intro.
-      destruct H as (S, (?, ?), ?).
-      exists (transp S); firstorder.
   Qed.
 
   Lemma multistep_reduction_closed:
@@ -305,9 +291,8 @@ Section BarbedRelations.
   Proof.
     (* Since there's a bisimulation S, its inverse is one as well. *)
     destruct 1 as (S, (?, ?), ?).
-    exists (transp S).
-    - split; auto.
-    - assumption.
+    exists (transp S); auto.
+    firstorder.
   Qed.
 
   Lemma barbed_bisimilarity_trans:
@@ -349,7 +334,20 @@ Section BarbedRelations.
           eauto.
         destruct H6 with x y n as (z, ?, ?); auto.
         exists z; eauto with cps.
-    - firstorder.
+    - exists y; auto.
+  Qed.
+
+  Lemma barbed_bisimilarity_is_a_bisimulation:
+    barbed_bisimulation barbed_bisimilarity.
+  Proof.
+    apply symmetric_barbed_simulation_is_bisimulation.
+    - split; do 5 intro.
+      + destruct H as (S, ((?, ?), ?), ?).
+        destruct H with a b c as (d, ?, ?); auto.
+        firstorder.
+      + destruct H as (S, ((?, ?), ?), ?).
+        firstorder.
+    - apply barbed_bisimilarity_sym.
   Qed.
 
   Lemma barbed_congruence_refl:
@@ -374,3 +372,175 @@ Section BarbedRelations.
   Qed.
 
 End BarbedRelations.
+
+Section Bisimulation.
+
+  Variable T: Type.
+  Variable L: Type.
+
+  Variable R: L -> relation T.
+
+  Variable tau: L.
+
+  Hypothesis is_tau_dec: forall l, { l = tau } + { l <> tau }.
+
+  Definition weak_transition (l: L): relation T :=
+    if is_tau_dec l then
+      rt(R tau)
+    else
+      comp rt(R tau) (R l).
+
+  Definition simulation (S: relation T): Prop :=
+    forall a b,
+    S a b ->
+    forall c l,
+    R l a c ->
+    exists2 d,
+    weak_transition l b d & S c d.
+
+  Definition bisimulation (S: relation T): Prop :=
+    simulation S /\ simulation (transp S).
+
+  Definition bisimilarity a b: Prop :=
+    exists2 S, bisimulation S & S a b.
+
+  Lemma weak_transition_immediate:
+    forall l a b,
+    R l a b -> weak_transition l a b.
+  Proof.
+    intros.
+    unfold weak_transition.
+    destruct is_tau_dec with l.
+    - destruct e; auto with cps.
+    - exists a; auto with cps.
+  Qed.
+
+  Lemma symmetric_simulation_is_bisimulation:
+    forall S,
+    simulation S ->
+    symmetric S ->
+    bisimulation S.
+  Proof.
+    split.
+    - assumption.
+    - do 6 intro.
+      destruct H with a b c l as (d, ?, ?); auto.
+      exists d; firstorder.
+  Qed.
+
+  Lemma weak_transition_silent:
+    forall l,
+    l = tau ->
+    weak_transition l = rt(R tau).
+  Proof.
+    unfold weak_transition; intros.
+    destruct is_tau_dec with l.
+    - reflexivity.
+    - firstorder.
+  Qed.
+
+  Lemma weak_transition_nonsilent:
+    forall l,
+    l <> tau ->
+    weak_transition l = comp rt(R tau) (R l).
+  Proof.
+    intros.
+    unfold weak_transition.
+    destruct is_tau_dec with l.
+    - firstorder.
+    - reflexivity.
+  Qed.
+
+  Lemma weak_transition_closed:
+    forall S,
+    simulation S ->
+    forall a b,
+    S a b ->
+    forall c l,
+    weak_transition l a c ->
+    exists2 d,
+    weak_transition l b d & S c d.
+  Proof.
+    intros until l.
+    assert (forall c, rt(R tau) a c ->
+      exists2 d, rt(R tau) b d & S c d).
+    - clear c; intros.
+      generalize dependent b.
+      induction H1; intros.
+      + destruct H with x b y tau as (z, ?, ?); auto.
+        rewrite weak_transition_silent in H2; auto.
+        exists z; eauto.
+      + exists b; auto with cps.
+      + edestruct IHclos_refl_trans1 as (w, ?, ?); eauto.
+        edestruct IHclos_refl_trans2 as (v, ?, ?); eauto.
+        exists v; eauto with cps.
+    - unfold weak_transition.
+      destruct is_tau_dec with l; intros.
+      + eapply H1; eauto.
+      + destruct H2 as (d, ?, ?).
+        edestruct H1 as (x, ?, ?); eauto.
+        destruct H with d x c l as (y, ?, ?); eauto.
+        rewrite weak_transition_nonsilent in H6; auto.
+        destruct H6 as (z, ?, ?).
+        exists y; auto.
+        exists z; eauto with cps.
+  Qed.
+
+  Lemma bisimilarity_refl:
+    reflexive bisimilarity.
+  Proof.
+    (* Same reasoning as for reflexivity on the barbed bisimilarity above. *)
+    exists eq; auto.
+    split; do 6 intro.
+    - destruct H.
+      exists c; auto.
+      apply weak_transition_immediate; auto.
+    - destruct H.
+      exists c; auto with cps.
+      apply weak_transition_immediate; auto.
+  Qed.
+
+  Lemma bisimilarity_sym:
+    symmetric bisimilarity.
+  Proof.
+    (* Same reasoning as for symmetry on the barbed bisimilarity above. *)
+    destruct 1 as (S, ?, ?).
+    exists (transp S); auto.
+    firstorder.
+  Qed.
+
+  Lemma bisimilarity_trans:
+    transitive bisimilarity.
+  Proof.
+    (* Same reasoning as for transitivity on the barbed bisimilarity above. *)
+    destruct 1 as (S1, (?, ?), ?).
+    destruct 1 as (S2, (?, ?), ?).
+    exists (comp S1 S2).
+    - split; do 6 intro.
+      + destruct H5 as (d, ?, ?).
+        destruct H with a d c l as (w, ?, ?); auto.
+        destruct weak_transition_closed with S2 d b w l as (v, ?, ?); auto.
+        exists v; auto.
+        exists w; auto.
+      + destruct H5 as (d, ?, ?).
+        destruct H3 with a d c l as (w, ?, ?); auto.
+        destruct weak_transition_closed with (transp S1) d b w l as (v, ?, ?);
+          auto.
+        exists v; auto.
+        exists w; auto.
+    - exists y; auto.
+  Qed.
+
+  Lemma bisimilarity_is_a_bisimulation:
+    bisimulation bisimilarity.
+  Proof.
+    apply symmetric_simulation_is_bisimulation.
+    - do 6 intro.
+      destruct H as (S, (?, ?), ?).
+      destruct H with a b c l as (d, ?, ?); auto.
+      exists d; auto.
+      firstorder.
+    - apply bisimilarity_sym.
+  Qed.
+
+End Bisimulation.
