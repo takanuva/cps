654923ed189f0f6eaad358d9d05d50270e14da1a
Paulo Torrens <paulotorrens@gnu.org>
2021-12-05 11:30:49 -0300

    Change the definition of neutral (things seem to make more sense now)

--- theories/Normalization.v
+++ theories/Normalization.v
@@ -9,6 +9,7 @@ Require Import Equality.
 Require Import Local.Prelude.
 Require Import Local.Syntax.
 Require Import Local.Metatheory.
+Require Import Local.Context.
 Require Import Local.AbstractRewriting.
 Require Import Local.Reduction.
 Require Import Local.TypeSystem.
@@ -75,47 +76,55 @@ Fixpoint count (t: pseudoterm): nat :=
     0
   end.
 
-Lemma count_is_well_founded:
-  well_founded (ltof _ count).
+Lemma sumup_count_is_well_founded:
+  well_founded (ltof _ (sumup count)).
 Proof.
   apply well_founded_ltof.
-Defined.
+Qed.
 
 Lemma count_arg:
   forall {t ts g},
-  t = negation (negation ts :: g) ->
-  count (negation ts) < count t.
+  t = (negation ts :: g) ->
+  sumup count ts < sumup count t.
 Proof.
-  intros.
-  rewrite H; simpl.
-  replace (negation ts :: g) with ([negation ts] ++ g); auto.
-  rewrite sumup_app; simpl; auto with arith.
-Qed.
+  (* Still not sure: ts ++ g, or simply ts? *)
+  admit.
+Admitted.
 
 Lemma count_ret:
   forall {t ts g},
-  t = negation (negation ts :: g) ->
-  count (negation g) < count t.
+  t = (negation ts :: g) ->
+  sumup count g < sumup count t.
 Proof.
-  intros.
-  rewrite H; simpl.
-  replace (negation ts :: g) with ([negation ts] ++ g); auto.
-  rewrite sumup_app; simpl; auto with arith.
-Qed.
+  admit.
+Admitted.
 
 Definition ARR ts (U V: pseudoterm -> Prop): pseudoterm -> Prop :=
   fun b =>
     forall c: pseudoterm,
     U c -> V (bind b ts c).
 
+(* A neutral term should not trigger a reduction interacting with its context.
+   E.g., in the lambda calculus, they are neither abstractions, which would
+   trigger a reduction with ([] x), nor <a, b>, which would trigger a reduction
+   with (pi1 []) or (pi2 []). Since the CPS calculus "works at a distance", we
+   would probably need something that DOESN'T jump to bound variables. *)
+Axiom neutral: env -> pseudoterm -> Prop.
+
 (* TODO: interpret ~(G, b)... by subst? *)
-Definition L: pseudoterm -> pseudoterm -> Prop :=
-  Fix count_is_well_founded (fun _ => pseudoterm -> Prop) (fun t f =>
+Definition L: env -> pseudoterm -> Prop :=
+  Fix sumup_count_is_well_founded (fun _ => pseudoterm -> Prop) (fun t f =>
     match t as x return (t = x -> pseudoterm -> Prop) with
-    (* Given ~(G, ~T)... *)
-    | negation (negation ts :: g) =>
+    (* Given (G, ~T)... *)
+    | negation ts :: g =>
       fun H =>
-        ARR ts (f _ (count_arg H)) (f _ (count_ret H))
+        ARR ts (fun c =>
+          (* Perhaps we'll need to fix this ts in the neutral... we don't really
+             care about jumps to variables which aren't bound in a continuation.
+             This is similar to how we dealt with regular terms in the residuals
+             theory. *)
+          neutral (ts ++ g) c /\ (f _ (count_arg H)) c)
+            (f _ (count_ret H))
     (* By default... *)
     | _ =>
       fun _ =>
@@ -124,8 +133,8 @@ Definition L: pseudoterm -> pseudoterm -> Prop :=
 
 Lemma L_composition:
   forall ts g,
-  L (negation (negation ts :: g)) =
-    ARR ts (L (negation ts)) (L (negation g)).
+  L (negation ts :: g) =
+    ARR ts (fun c => neutral (ts ++ g) c /\ L ts c) (L g).
 Proof.
   intros.
   unfold L.
@@ -134,7 +143,6 @@ Proof.
     reflexivity.
   - intros.
     destruct x; simpl; auto.
-    destruct ts0; simpl; auto.
     destruct p; simpl; auto.
     do 2 rewrite H; auto.
 Defined.
@@ -142,15 +150,7 @@ Defined.
 Definition candidate: Type :=
   pseudoterm -> Prop.
 
-(* A neutral term should no trigger a reduction interacting with its context.
-   I.e., in the lambda calculus, they are neither abstractions, which would
-   trigger a reduction with ([] x), nor <a, b>, which would trigger a reduction
-   with (pi1 []) or (pi2 []). Since the CPS calculus "works at a distance", we
-   would probably need something that DOESN'T jump to bound variables. So I'll
-   probably need to index this by the (size of the) environment. *)
-Axiom neutral: pseudoterm -> Prop.
-
-Record reducible (P: candidate): Prop := {
+Record reducible g (P: candidate): Prop := {
   cr1:
     forall e,
     P e -> SN step e;
@@ -159,25 +159,40 @@ Record reducible (P: candidate): Prop := {
     P a -> [a => b] -> P b;
   cr3:
     forall a,
-    (* Since the CPS calculus is non-erasing, do we really need to restrict
-       ourselves to neutral terms here...? *)
-    neutral a -> (forall b, [a => b] -> P b) -> P a
+    (* Since the CPS calculus seems to be non-erasing, do we really need to
+       restrict ourselves to neutral terms here...? *)
+    neutral g a -> (forall b, [a => b] -> P b) -> P a
 }.
 
 Lemma cr2_star:
-  forall P,
-  reducible P ->
+  forall g P,
+  reducible g P ->
   forall a b,
   P a -> [a =>* b] -> P b.
 Proof.
   induction 3.
-  - apply cr2 with x; auto.
+  - apply cr2 with g x; auto.
   - assumption.
   - auto.
 Qed.
 
+Lemma cr4:
+  forall g P,
+  reducible g P ->
+  forall e,
+  normal step e -> neutral g e -> P e.
+Proof.
+  intros.
+  apply cr3 with g; intros.
+  - assumption.
+  - assumption.
+  - exfalso.
+    firstorder.
+Qed.
+
 Lemma reducible_SN:
-  reducible (SN step).
+  forall g,
+  reducible g (SN step).
 Proof.
   constructor; intros.
   - assumption.
@@ -189,73 +204,111 @@ Qed.
 
 Lemma reducible_ARR:
   forall g ts,
-  reducible (L (negation g)) ->
-  reducible (L (negation ts)) ->
-  reducible (L (negation (negation ts :: g))).
+  reducible g (L g) ->
+  reducible ts (L ts) ->
+  reducible (negation ts :: g) (L (negation ts :: g)).
 Proof.
   constructor; intros.
   - rewrite L_composition in H1.
     unfold ARR in H1.
-    cut (exists c, L (negation ts) c).
-    + destruct 1 as (c, ?H).
-      apply SN_bind_left with ts c.
-      apply cr1 with (L (negation g)); auto.
+    assert (exists2 c, L ts c & neutral (ts ++ g) c).
     + (* We should add a free jump here, right...? *)
       admit.
+    + destruct H2 as (c, ?, ?).
+      apply SN_bind_left with ts c.
+      apply cr1 with g (L g); auto.
   - rewrite L_composition in H1 |- *.
     unfold ARR in H1 |- *; intros.
-    apply cr2 with (bind a ts c); auto.
-    apply step_bind_left; auto.
+    apply cr2 with g (bind a ts c); auto.
+    auto with cps.
   - rewrite L_composition in H2 |- *.
     unfold ARR in H2 |- *; intros.
+    destruct H3.
     assert (SN step c).
-    + eapply cr1 with (L (negation ts)); auto.
-    + induction H4; clear H4.
-      apply cr3; intros.
+    + eapply cr1 with ts (L ts); auto.
+    + induction H5; clear H5.
+      unfold transp in H6.
+      apply cr3 with g; intros.
       * assumption.
-      * (* Oh no! Are we really neutral here?
-
-           We'll probably have to change candidates from sets of terms to
-           relations between environments and terms. Could we derive then that x
-           is neutral based on that, e.g., such that a { k<xs: ts> = x } is
-           neutral in G? *)
+      * (* NOW this seems correct to me! Check H1 and H3... *)
         admit.
-      * dependent destruction H4.
-        -- (* It is neutral, so we CAN'T have a redex here! *)
-           admit.
-        -- apply H2; auto.
-        -- apply H5; auto.
-           apply cr2 with x; auto.
+      * dependent destruction H5.
+        --- (* It is neutral, so we CAN'T have a redex here! *)
+            exfalso.
+            (* We can derive that from H1! *)
+            admit.
+        --- apply H2; auto.
+        --- apply H6; auto.
+            +++ (* Sure, from H3 and H5... *)
+                admit.
+            +++ apply cr2 with ts x; auto.
 Admitted.
 
 Lemma reducible_L:
-  forall t,
-  reducible (L t).
+  forall g,
+  reducible g (L g).
 Proof.
   intros.
-  induction count_is_well_founded with t.
+  induction sumup_count_is_well_founded with g.
   clear H; rename H0 into H; unfold ltof in H.
   (* Ok, start wordering about the type... *)
   destruct x; try exact reducible_SN.
-  destruct ts; try exact reducible_SN.
-  destruct p; try exact reducible_SN.
-  (* "Arrow types", in a way... *)
-  rename ts0 into ts, ts into g.
-  apply reducible_ARR.
-  - apply H.
-    eapply count_ret; eauto.
-  - apply H.
-    eapply count_arg; eauto.
-Qed.
+  - admit.
+  - destruct p; try exact reducible_SN.
+    + admit.
+    + admit.
+    + admit.
+    + admit.
+    + admit.
+    + (* "Arrow types", in a way... *)
+      eapply reducible_ARR.
+      * apply H.
+        eapply count_ret; eauto.
+      * apply H.
+        eapply count_arg; eauto.
+    + admit.
+    + admit.
+Admitted.
 
 Lemma SN_L:
-  forall t e,
-  L t e -> SN step e.
+  forall g e,
+  L g e -> SN step e.
 Proof.
   intros.
-  apply cr1 with (L t); auto.
+  apply cr1 with g (L g); auto.
   apply reducible_L.
 Qed.
+
+Goal
+  forall A B C,
+  let g :=
+    [negation A; negation B; negation C]
+  in L g void.
+Proof.
+  (* Lets see how things unfold... *)
+  intros.
+  unfold g.
+  rewrite L_composition.
+  unfold ARR; intros.
+  rewrite L_composition.
+  unfold ARR; intros.
+  rewrite L_composition; intros.
+  unfold ARR; intros.
+  rename c into a, c0 into b, c1 into c.
+  destruct H.
+  destruct H0.
+  destruct H1.
+  rewrite app_nil_r in H1.
+  unfold L.
+  rewrite Fix_eq; [|
+    intros;
+    destruct x; auto;
+    destruct p; auto;
+    do 2 rewrite H5;
+    reflexivity ].
+  (* Hmm...! *)
+  admit.
+Admitted.
 
 (*
 Lemma fundamental:
