6d02613160df3d563fd343b73e93880431d5f6d1
Paulo Torrens <paulotorrens@gnu.org>
2022-05-06 16:19:29 -0300

    Write some musings down before I forget about them

--- theories/FullAbstraction.v
+++ theories/FullAbstraction.v
@@ -6,3 +6,11 @@ Require Import Local.Prelude.
 Require Import Local.Syntax.
 Require Import Local.Context.
 Require Import Local.Simulation.
+
+(* It is most probably not possible to achieve full abstraction as terms in the
+   CPS-calculus are allowed to discard and duplicate continuations; a simple
+   example using call/cc might be enough (there's something similar in one of
+   Ahmed's papers). We might try to prove that 1) it's really not possible, and
+   2) that it is indeed possible if we restrict ourselves to the subset of terms
+   that are free of control effects (which is actually a subcategory, as it is
+   described by Thielecke). This restriction may be done syntactically. *)
