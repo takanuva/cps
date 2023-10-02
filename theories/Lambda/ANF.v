e0e25a1ff30e71c3b20211ecb4a5d9d972773ff7
Paulo Torrens <paulotorrens@gnu.org>
2023-10-02 04:18:07 +0100

    Some musings about ANF (WIP, incomplete)

--- theories/Lambda/ANF.v
+++ theories/Lambda/ANF.v
@@ -65,12 +65,12 @@ Inductive anf: relation term :=
      split in two cases, since allowing E to be just a hole would lead to a
      reflexive relation. *)
   | anf_beta_lift_left:
-    (* (\x.M) N L -> (\x.M L) N *)
+    (* Sigma-1: (\x.M) N L -> (\x.M L) N *)
     forall t M N L,
     anf (APP (APP (ABS t M) N) L)
         (APP (ABS t (APP M (lift 1 0 L))) N)
   | anf_beta_lift_right:
-    (* V ((\x.M) N) -> (\x.V M) N *)
+    (* Sigma-3: V ((\x.M) N) -> (\x.V M) N *)
     forall V t M N,
     value V ->
     anf (APP V (APP (ABS t M) N))
@@ -86,7 +86,8 @@ Inductive anf: relation term :=
   | anf_beta_id:
     forall t M,
     anf (APP (ABS t 0) M) M
-  (* Beta-omega: (\x.E[y x]) M -> E[y M], if x not free in E[y]. *)
+  (* Beta-omega: (\x.E[y x]) M -> E[y M], if x not free in E[y]. Is there a way
+     to factor this so that we don't need the E...? *)
   | anf_beta_omega:
     forall t E x y M,
     evaluation E ->
@@ -94,6 +95,139 @@ Inductive anf: relation term :=
     y <> context_bvars E ->
     anf (APP (ABS t (context_lift 1 0 E (APP (lift 1 x y) x))) M)
         (E (APP y (lift x 0 M))).
+
+(* Count the number of applications on the left-hand side of an application. *)
+
+Fixpoint unnamed_subterms e: nat :=
+  match e with
+  | bound _ =>
+    0
+  | abstraction _ b =>
+    unnamed_subterms b
+  | application (application _ _ as f) x =>
+    1 + unnamed_subterms f + unnamed_subterms x
+  | application f x =>
+    unnamed_subterms f + unnamed_subterms x
+  end.
+
+Lemma unnamed_subterms_lift:
+  forall e i k,
+  unnamed_subterms (lift i k e) = unnamed_subterms e.
+Proof.
+  induction e; simpl; intros.
+  - destruct (le_gt_dec k n); auto.
+  - apply IHe.
+  - rewrite IHe1, IHe2.
+    destruct e1; simpl; auto.
+    destruct (le_gt_dec k n); auto.
+Qed.
+
+Lemma unnamed_subterms_context_lift:
+  forall h i k e,
+  unnamed_subterms (context_lift i k h e) = unnamed_subterms (h e).
+Proof.
+  induction h; simpl; intros.
+  - reflexivity.
+  - apply IHh.
+  - rewrite unnamed_subterms_lift.
+    rewrite IHh; destruct h; simpl; auto.
+  - rewrite unnamed_subterms_lift.
+    rewrite IHh; destruct f; simpl; auto.
+    destruct (le_gt_dec k n); auto.
+Qed.
+
+Notation cmp a b := (a = b \/ a > b).
+
+Goal
+  forall a b,
+  anf a b ->
+  cmp (unnamed_subterms a) (unnamed_subterms b).
+Proof.
+  induction 1; intros.
+  (* Case: eta-v. *)
+  - left.
+    destruct H; simpl; lia.
+  (* Case: sigma-1. *)
+  - destruct M; simpl.
+    + right.
+      rewrite unnamed_subterms_lift.
+      lia.
+    + right.
+      rewrite unnamed_subterms_lift.
+      lia.
+    + (* Here it decreases! *)
+      left.
+      rewrite unnamed_subterms_lift.
+      lia.
+  (* Case: sigma-3. *)
+  - left.
+    destruct H; simpl.
+    + auto.
+    + rewrite unnamed_subterms_lift.
+      lia.
+  (* Case: beta-flat. *)
+  - (* Here it decreases! *)
+    right; simpl.
+    rewrite unnamed_subterms_lift.
+    lia.
+  (* Case: beta-id. *)
+  - left; simpl; auto.
+  (* Case: beta-omega. *)
+  - (* Aw crap... *)
+    left; simpl.
+    rewrite unnamed_subterms_context_lift.
+    (* We don't actually care about beta in here... *)
+    clear H0 H1.
+    induction H; simpl.
+    + rewrite unnamed_subterms_lift.
+      destruct (le_gt_dec x y); auto.
+    + rewrite <- IHevaluation.
+      (* I was not expecting this to work, but... hey! It did! *)
+      destruct f; simpl; lia.
+    + rewrite <- IHevaluation.
+      destruct H0; simpl; lia.
+Qed.
+
+(* Count the number of beta redexes within applications. *)
+
+Definition is_beta e :=
+  match e with
+  | application (abstraction _ _) _ =>
+    1
+  | _ =>
+    0
+  end.
+
+Lemma is_beta_lift:
+  forall e i k,
+  is_beta (lift i k e) = is_beta e.
+Proof.
+  destruct e; simpl; intros.
+  - destruct (le_gt_dec k n); auto.
+  - reflexivity.
+  - destruct e1; simpl; auto.
+    destruct (le_gt_dec k n); auto.
+Qed.
+
+Fixpoint inner_computations e: nat :=
+  match e with
+  | bound _ =>
+    0
+  | abstraction _ b =>
+    inner_computations b
+  | application f x =>
+    is_beta f + is_beta x + inner_computations f + inner_computations x
+  end.
+
+(*
+  Rules 1, 5 and 6 decrease the term size, while not changing the number of
+  inner lets nor the number of unnamed stuff.
+
+  Rules 2 and 3 decrease the number of inner lets, while not changing the number
+  of unnamed stuff.
+
+  Rule 4 decreases the number of unnamed stuff.
+*)
 
 Goal
   forall e1 e2,
