0c203c52228da50689b7dcc6ade0b1fc1c71ea7d
Paulo Torrens <paulotorrens@gnu.org>
2023-11-04 22:41:22 +0000

    Some experimentation with substitutions...

--- theories/Substitution.v
+++ theories/Substitution.v
@@ -2,28 +2,18 @@
 (*   Copyright (c) 2019--2023 - Paulo Torrens <paulotorrens AT gnu DOT org>   *)
 (******************************************************************************)
 
-Set Implicit Arguments.
 Require Import Lia.
 Require Import List.
 Require Import Arith.
 Require Import Relations.
 Require Import Morphisms.
-(* TODO: remove me. *)
-Require Import String.
+Set Implicit Arguments.
 
-(* We'll make a database just for rewriting substitution stuff, so that we can
-   easily add rewriting rules to it and rewrite just that if we wish. *)
+(* *)
+
 Create HintDb sigma.
 
-(* We define in here renaming, well-scoped substitution, and an algebra for
-   substitution (or de Bruijn algebra), inspired by the work of Schafer et al.
-   on the autosubst library, particularly by the Completeness and Decidability
-   of de Bruijn Substitution Algebra in Coq paper. This was inspired by the
-   definition of the lambda sigma calculus, from Abadi et al., introduced in
-   their Explicit Substitutions paper. By using an algebra like this a few
-   proofs should become easier, like the completeness for machine semantics. See
-   also Stark et al.'s Autosubst 2: Reasoning with Multi-sorted de Bruijn Terms
-   and Vector Substitutions paper. *)
+(* *)
 
 Section DeBruijn.
 
@@ -33,7 +23,22 @@ Section DeBruijn.
     var:
       nat -> X;
     traverse:
-      (nat -> nat -> X) -> nat -> X -> X
+      (nat -> nat -> X) -> nat -> X -> X;
+    traverse_var:
+      forall f k n,
+      traverse f k (var n) = f k n;
+    traverse_ids:
+      forall k x,
+      traverse (fun _ n => var n) k x = x;
+    traverse_ext:
+      forall f g,
+      (forall k n, f k n = g k n) ->
+      forall x k,
+      traverse f k x = traverse g k x;
+    traverse_fun:
+      forall f g x k j,
+      traverse f k (traverse g j x) =
+        traverse (fun i n => traverse f (i + k - j) (g i n)) j x
   }.
 
   Context `{deBruijn}.
@@ -41,232 +46,284 @@ Section DeBruijn.
   #[nonuniform]
   Local Coercion var: nat >-> X.
 
-  Inductive substitution: Set :=
+  Inductive substitution: Type :=
     | subst_id
     | subst_lift (i: nat)
-    | subst_cons (x: X) (s: substitution)
+    | subst_cons (y: X) (s: substitution)
     | subst_comp (s: substitution) (t: substitution).
+
+  Coercion subst_lift: nat >-> substitution.
 
   Local Notation I := subst_id.
   Local Notation L i := (subst_lift i).
-  Local Notation S := (L 1).
   Local Notation "( x1 , .. , xn , s )" :=
     (subst_cons x1 .. (subst_cons xn s) ..) (at level 0, right associativity).
-  Local Notation "s 'o' r" :=
-    (subst_comp s r) (at level 11, left associativity).
+  Local Notation "s 'o' t" :=
+    (subst_comp s t) (at level 11, left associativity).
 
-  Local Notation up s :=
-    (0, s o L 1).
+  Definition __lift (i: nat): nat -> X -> X :=
+    traverse (fun k n =>
+      if le_gt_dec k n then
+        var (i + n)
+      else
+        var n).
 
-  (* Definition lift (i: nat) (k: nat) (n: nat): nat :=
-    if le_gt_dec k n then
-      i + n
-    else
-      n. *)
+  Tactic Notation "simplify" "decidable" "equality" :=
+    match goal with
+    | |- context H [le_gt_dec ?m ?n] =>
+      (destruct (le_gt_dec m n) as [ ? | _ ];
+        [ exfalso; lia | idtac ]) +
+      (destruct (le_gt_dec m n) as [ _ | ? ];
+        [ idtac | exfalso; lia ])
+    end.
 
-  Axiom TODO: forall {T}, string -> T.
+  Goal
+    forall i j k l x,
+    k <= l + j ->
+    l <= k ->
+    __lift i k (__lift j l x) = __lift (i + j) l x.
+  Proof.
+    intros.
+    unfold __lift.
+    rewrite traverse_fun.
+    apply traverse_ext; clear x; intros.
+    rename k0 into p.
+    destruct (le_gt_dec p n); simpl.
+    - rewrite traverse_var.
+      simplify decidable equality.
+      f_equal; lia.
+    - rewrite traverse_var.
+      simplify decidable equality.
+      reflexivity.
+  Qed.
 
-  Fixpoint subst_apply (s: substitution) (k: nat) (n: nat): X :=
+  Fixpoint apply_subst (s: substitution) (k: nat) (n: nat): X :=
     if le_gt_dec k n then
       match s with
       | I =>
-        n
+        var n
       | L i =>
-        i + n
+        var (i + n)
       | (y, s) =>
-        TODO "cons"
-      | s o r =>
-        TODO "comp"
+        let x := match n - k with
+                 | 0 => y
+                 | S m => apply_subst s 0 m
+                 end
+        in __lift k 0 x
+      | s o t =>
+        traverse (apply_subst t) k (apply_subst s k n)
       end
     else
-      n.
+      var n.
 
-  Notation instantiate_rec s :=
-    (traverse (subst_apply s)).
-
-  Definition instantiate (s: substitution): X -> X :=
-    instantiate_rec s 0.
-
-  Coercion instantiate: substitution >-> Funclass.
-
-  Lemma traverse_subst_instantiate:
-    forall s x,
-    instantiate_rec s 0 x = s x.
+  Goal
+    forall i k x,
+    (* We have intensional equality between these. *)
+    __lift i k x = traverse (apply_subst i) k x.
   Proof.
     intros.
     reflexivity.
   Qed.
 
-  Definition subst_equiv: relation substitution :=
-    fun s r =>
-      forall x, s x = r x.
+  Definition instantiate (s: substitution): X -> X :=
+    traverse (apply_subst s) 0.
 
-  (*
-    Instantiation laws:
+  Coercion instantiate: substitution >-> Funclass.
 
-      - 0[y, s] = y
-      - (1 + n)[y, s] = n[s]
-      - n[S^i] = i + n
+  Notation up s :=
+    (0, s o 1).
 
-    Monad laws:
+  (* TODO: should we delcare this as a setoid...? *)
 
-      - e[I] = e
-      - e[s][r] = e[s >> r]
+  Definition subst_equiv (s: substitution) (t: substitution): Prop :=
+    forall x: X,
+    s x = t x.
 
-    Algebraic laws:
+  Lemma lift_zero_e_equals_e:
+    forall k x,
+    __lift 0 k x = x.
+  Proof.
+    intros.
+    unfold __lift.
+    replace x with (traverse (fun _ n => n) k x) at 2.
+    - apply traverse_ext; simpl; intros.
+      now destruct (le_gt_dec k0 n).
+    - apply traverse_ids.
+  Qed.
 
-      - 0 .: S = I (could be derived from the eta-like law and the monad laws)
-      - S >> (e .: s) = s
-      - I >> s = s
-      - s >> I = s
-      - (s >> r) >> u = s >> (r >> u) (we need to reverse this one!)
-      - (e .: s) >> r = e[r] .: (s >> r)
-      - (0[s], S >> s) = s (is this an eta law...?)
-
-    We also need extra care since we want to simplify stuff by using the up
-    combinator... so in normal form, we'll associate composition to the left
-    instead of to the right. Also, as we are taking the lift primitive indexed
-    by a number to optimize stuff a bit, we probably need laws such as, e.g.,
-    (s >> L n) >> L m = s >> L (n + m). As s can be I, L n >> L m = L (n + m)
-    too, we probably need an extra reduction law for that. Ideally we would want
-    to prove confluence and completeness for this eventually, but this is not
-    important right now.
-  *)
-
-  Lemma instantiate_cons_zero:
+  Goal
     forall y s,
     (y, s) 0 = y.
   Proof.
     intros.
-    unfold instantiate.
-    admit.
-  Admitted.
+    unfold instantiate; simpl.
+    rewrite traverse_var; simpl.
+    now rewrite lift_zero_e_equals_e.
+  Qed.
 
-  Lemma instantiate_cons_succ:
+  Goal
     forall y s n,
-    (y, s) (1 + n) = s n.
+    (y, s) (S n) = s n.
   Proof.
-    admit.
-  Admitted.
+    intros.
+    unfold instantiate; simpl.
+    do 2 rewrite traverse_var; simpl.
+    now rewrite lift_zero_e_equals_e.
+  Qed.
 
-  Lemma instantiate_lift:
-    forall (n: nat) i,
+  Goal
+    forall (i n: nat),
     L i n = i + n.
   Proof.
-    admit.
-  Admitted.
+    intros.
+    unfold instantiate; simpl.
+    rewrite traverse_var; simpl.
+    reflexivity.
+  Qed.
 
-  Lemma instantiate_id:
+  Goal
     forall x,
     I x = x.
   Proof.
-    admit.
-  Admitted.
+    intros.
+    (* This is not obvious, but it follows by definition! *)
+    apply lift_zero_e_equals_e.
+  Qed.
 
-  Lemma instantiate_comp:
-    forall x s r,
-    (s o r) x = r (s x).
+  Goal
+    forall s t x,
+    (s o t) x = t (s x).
   Proof.
-    admit.
-  Admitted.
+    intros.
+    unfold instantiate.
+    rewrite traverse_fun; simpl.
+    apply traverse_ext; clear x; intros.
+    replace (k + 0 - 0) with k; try lia.
+    destruct s;
+    simpl;
+    destruct (le_gt_dec k n);
+    simpl;
+    auto;
+    rewrite traverse_var;
+    destruct t;
+    simpl;
+    destruct (le_gt_dec k n);
+    try lia; auto.
+  Qed.
 
-  (* TODO: do we want to import SetoidClass? *)
-
-  Local Notation "s == r" := (subst_equiv s r) (at level 70, no associativity).
-
-  Lemma subst_cons_lift_simpl:
-    (0, S) == I.
+  Goal
+    subst_equiv 0 I.
   Proof.
-    admit.
-  Admitted.
+    intros x.
+    unfold instantiate.
+    apply traverse_ext; clear x; intros.
+    simpl; reflexivity.
+  Qed.
 
-  Lemma subst_lift_cons_simpl:
+  Goal
+    subst_equiv (0, 1) I.
+  Proof.
+    intros x.
+    unfold instantiate; simpl.
+    apply traverse_ext; clear x; intros.
+    destruct (le_gt_dec k n).
+    - remember (n - k) as m.
+      destruct m.
+      + unfold __lift.
+        rewrite traverse_var; simpl.
+        f_equal; lia.
+      + unfold __lift.
+        rewrite traverse_var; simpl.
+        f_equal; lia.
+    - reflexivity.
+  Qed.
+
+  Goal
     forall y s,
-    S o (y, s) == s.
+    subst_equiv (1 o (y, s)) s.
   Proof.
-    admit.
+    intros y s x.
+    unfold instantiate; simpl.
+    apply traverse_ext; clear x; intros.
+    destruct (le_gt_dec k n).
+    - rewrite traverse_var.
+      simplify decidable equality.
+      (* TODO: should the above simplify this match as well...? *)
+      remember (S n - k) as m.
+      destruct m.
+      + exfalso; lia.
+      + replace m with (n - k) by lia.
+        clear Heqm m.
+        (* This law is valid! *)
+        admit.
+    - destruct s; simpl.
+      + now simplify decidable equality.
+      + now simplify decidable equality.
+      + now simplify decidable equality.
+      + now simplify decidable equality.
   Admitted.
 
-  Lemma subst_comp_left_identity:
+  Goal
     forall s,
-    I o s == s.
+    subst_equiv (I o s) s.
   Proof.
     admit.
   Admitted.
 
-  Lemma subst_comp_right_identity:
+  Goal
     forall s,
-    s o I == s.
+    subst_equiv (s o I) s.
   Proof.
     admit.
   Admitted.
 
-  Lemma subst_comp_assoc:
-    forall s r t,
-    s o (r o t) == s o r o t.
+  Goal
+    forall s t u,
+    subst_equiv (s o (t o u)) (s o t o u).
   Proof.
     admit.
   Admitted.
 
-  Lemma subst_comp_cons_distr:
-    forall y s r,
-    (y, s) o r == (r y, s o r).
+  Goal
+    forall y s t,
+    subst_equiv ((y, s) o t) (t y, s o t).
   Proof.
     admit.
   Admitted.
 
-  Lemma subst_cons_eta:
-    forall s: substitution,
-    (s 0, S o s) == s.
+  Goal
+    (* This rule is useful for rewriting as that we can deal with metavariables
+       in the proof. *)
+    forall (s: substitution),
+    subst_equiv (s 0, 1 o s) s.
   Proof.
     admit.
   Admitted.
-
-  (* ---------------------------------------------------------------------- *)
-
-  Global Instance subst_equiv_equivalence:
-    Equivalence subst_equiv.
-  Proof.
-    split.
-    - intros s x.
-      reflexivity.
-    - intros s r ? x.
-      symmetry; auto.
-    - intros s r t ? ? x.
-      transitivity (r x); auto.
-  Qed.
-
-  Global Instance subst_equiv_cons_proper:
-    Proper (eq ==> subst_equiv ==> subst_equiv) subst_cons.
-  Proof.
-    intros y _ () s r ? x.
-    admit.
-  Admitted.
-
-  Global Instance subst_equiv_comp_proper:
-    Proper (subst_equiv ==> subst_equiv ==> subst_equiv) subst_comp.
-  Proof.
-    intros s r ? t u ? x.
-    admit.
-  Admitted.
-
-  Global Instance instantiate_subst_equiv_proper:
-    Proper (subst_equiv ==> eq ==> eq) instantiate.
-  Proof.
-    intros s r ? x _ ().
-    apply H0.
-  Qed.
-
-  (* ---------------------------------------------------------------------- *)
 
 End DeBruijn.
 
-Global Opaque instantiate.
+(* Global Opaque instantiate. *)
 
 Arguments subst_id {X}.
 Arguments subst_lift {X}.
 Arguments subst_cons {X}.
 Arguments subst_comp {X}.
+
+(*
+  Global Hint Rewrite traverse_subst_instantiate: subst.
+  Global Hint Rewrite instantiate_cons_zero: subst.
+  Global Hint Rewrite instantiate_cons_succ: subst.
+  Global Hint Rewrite instantiate_lift: subst.
+  Global Hint Rewrite instantiate_id: subst.
+  Global Hint Rewrite instantiate_comp: subst.
+  Global Hint Rewrite subst_cons_lift_simpl: subst.
+  Global Hint Rewrite subst_lift_cons_simpl: subst.
+  Global Hint Rewrite subst_comp_left_identity: subst.
+  Global Hint Rewrite subst_comp_right_identity: subst.
+  Global Hint Rewrite subst_comp_assoc: subst.
+  Global Hint Rewrite subst_comp_cons_distr: subst.
+  Global Hint Rewrite subst_cons_eta: subst.
+*)
 
 (* -------------------------------------------------------------------------- *)
 
@@ -280,5 +337,41 @@ Require Import Local.Machine.
 
 Global Instance pseudoterm_deBruijn: deBruijn pseudoterm.
 Proof.
-  apply (Build_deBruijn bound traverse).
-Defined.
+  apply (Build_deBruijn bound traverse); intros.
+  - simpl.
+    reflexivity.
+  - generalize dependent k.
+    induction x using pseudoterm_deepind; simpl; auto; intros.
+    + f_equal.
+      list induction over H.
+    + f_equal; auto.
+      list induction over H.
+    + f_equal; auto.
+      list induction over H.
+  - generalize dependent k.
+    induction x using pseudoterm_deepind; simpl; auto; intros.
+    + f_equal.
+      (* TODO: make list induction rewrite a few things as well... *)
+      list induction over H0.
+      now do 2 rewrite traverse_list_length.
+    + f_equal; auto.
+      list induction over H0.
+    + f_equal; auto.
+      list induction over H0.
+      now do 2 rewrite traverse_list_length.
+  - generalize dependent j.
+    generalize dependent k.
+    induction x using pseudoterm_deepind; simpl; auto; intros.
+    + f_equal; lia.
+    + f_equal.
+      list induction over H.
+      do 3 rewrite traverse_list_length.
+      rewrite H.
+      admit.
+    + f_equal; auto.
+      list induction over H.
+    + f_equal; auto.
+      * admit.
+      * admit.
+      * admit.
+Admitted.
