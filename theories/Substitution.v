9ad972fe2b9d7894d6e0bd701da73d369ce2aade
Paulo Torrens <paulotorrens@gnu.org>
2023-11-27 13:40:21 +0000

    Some reworking on substitutions (WIP)

--- theories/Substitution.v
+++ theories/Substitution.v
@@ -7,11 +7,24 @@ Require Import List.
 Require Import Arith.
 Require Import Relations.
 Require Import Morphisms.
+
+Import ListNotations.
 Set Implicit Arguments.
 
 (* *)
 
 Create HintDb sigma.
+
+(* TODO: move this tactic! Also... TODO: use this tactic more! *)
+
+Tactic Notation "simplify" "decidable" "equality" :=
+  match goal with
+  | |- context H [le_gt_dec ?m ?n] =>
+    (destruct (le_gt_dec m n) as [ ? | _ ];
+      [ exfalso; lia | idtac ]) +
+    (destruct (le_gt_dec m n) as [ _ | ? ];
+      [ idtac | exfalso; lia ])
+  end.
 
 (* *)
 
@@ -24,7 +37,7 @@ Section DeBruijn.
       nat -> X;
     traverse:
       (nat -> nat -> X) -> nat -> X -> X;
-    traverse_var:
+    (* traverse_var:
       forall f k n,
       traverse f k (var n) = f k n;
     traverse_ids:
@@ -38,7 +51,7 @@ Section DeBruijn.
     traverse_fun:
       forall f g x k j,
       traverse f k (traverse g j x) =
-        traverse (fun i n => traverse f (i + k - j) (g i n)) j x
+        traverse (fun i n => traverse f (i + k - j) (g i n)) j x *)
   }.
 
   Context `{deBruijn}.
@@ -46,45 +59,44 @@ Section DeBruijn.
   #[nonuniform]
   Local Coercion var: nat >-> X.
 
+  (* TODO: we may want to mess around. We actually only need lift and cons in
+     here... id and comp are derivable. We could move those out, then we'd get
+     something similar to the primitives de Bruijn used. Or we could move up
+     into here to have something similar to the confluent sigma calculus... we
+     could also move app in here, as it's useful for parallel substitution. So
+     this will be a problem for future me (I'm so sorry). *)
+
   Inductive substitution: Type :=
     | subst_id
     | subst_lift (i: nat)
     | subst_cons (y: X) (s: substitution)
     | subst_comp (s: substitution) (t: substitution).
 
-  Coercion subst_lift: nat >-> substitution.
+  Fixpoint subst_upn (i: nat) (s: substitution): substitution :=
+    match i with
+    | 0 =>
+      s
+    | S i =>
+      subst_cons 0 (subst_comp (subst_upn i s) (subst_lift 1))
+    end.
 
-  Local Notation I := subst_id.
-  Local Notation L i := (subst_lift i).
-  Local Notation "( x1 , .. , xn , s )" :=
-    (subst_cons x1 .. (subst_cons xn s) ..) (at level 0, right associativity).
-  Local Notation "s 'o' t" :=
-    (subst_comp s t) (at level 11, left associativity).
+  Definition subst_app (xs: list X) (s: substitution): substitution :=
+    fold_right subst_cons s xs.
 
-  Definition __lift (i: nat): nat -> X -> X :=
+  Definition lift (i: nat): nat -> X -> X :=
     traverse (fun k n =>
       if le_gt_dec k n then
         var (i + n)
       else
         var n).
 
-  Tactic Notation "simplify" "decidable" "equality" :=
-    match goal with
-    | |- context H [le_gt_dec ?m ?n] =>
-      (destruct (le_gt_dec m n) as [ ? | _ ];
-        [ exfalso; lia | idtac ]) +
-      (destruct (le_gt_dec m n) as [ _ | ? ];
-        [ idtac | exfalso; lia ])
-    end.
-
-  Goal
+  (* Goal
     forall i j k l x,
     k <= l + j ->
     l <= k ->
-    __lift i k (__lift j l x) = __lift (i + j) l x.
+    lift i k (lift j l x) = lift (i + j) l x.
   Proof.
-    intros.
-    unfold __lift.
+    intros; unfold lift.
     rewrite traverse_fun.
     apply traverse_ext; clear x; intros.
     rename k0 into p.
@@ -95,283 +107,81 @@ Section DeBruijn.
     - rewrite traverse_var.
       simplify decidable equality.
       reflexivity.
-  Qed.
+  Qed. *)
 
   Fixpoint apply_subst (s: substitution) (k: nat) (n: nat): X :=
     if le_gt_dec k n then
       match s with
-      | I =>
+      | subst_id =>
         var n
-      | L i =>
+      | subst_lift i =>
         var (i + n)
-      | (y, s) =>
+      | subst_cons y s =>
         let x := match n - k with
                  | 0 => y
                  | S m => apply_subst s 0 m
                  end
-        in __lift k 0 x
-      | s o t =>
+        in lift k 0 x
+      | subst_comp s t =>
         traverse (apply_subst t) k (apply_subst s k n)
       end
     else
       var n.
 
+  Local Notation instantiate_rec s k :=
+    (traverse (apply_subst s) k).
+
+  Definition instantiate (s: substitution): X -> X :=
+    instantiate_rec s 0.
+
+  Global Coercion instantiate: substitution >-> Funclass.
+
+  Definition test (x: X) (s: substitution): X :=
+    instantiate s x.
+
+  #[nonuniform]
+  Global Coercion test: X >-> Funclass.
+
   Goal
     forall i k x,
     (* We have intensional equality between these. *)
-    __lift i k x = traverse (apply_subst i) k x.
+    lift i k x = instantiate_rec (subst_lift i) k x.
   Proof.
-    intros.
-    reflexivity.
+    auto.
   Qed.
-
-  Definition instantiate (s: substitution): X -> X :=
-    traverse (apply_subst s) 0.
-
-  Coercion instantiate: substitution >-> Funclass.
-
-  Notation up s :=
-    (0, s o 1).
 
   (* TODO: should we delcare this as a setoid...? *)
 
   Definition subst_equiv (s: substitution) (t: substitution): Prop :=
-    forall x: X,
-    s x = t x.
+    (* TODO: It should be a sufficient and necessary condition to check only for
+       vars in here, so this would simplify stuff. *)
+    forall x,
+    instantiate s x = instantiate t x.
 
-  Lemma lift_zero_e_equals_e:
+  (* Lemma lift_zero_e_equals_e:
     forall k x,
-    __lift 0 k x = x.
+    lift 0 k x = x.
   Proof.
-    intros.
-    unfold __lift.
+    intros; unfold lift.
     replace x with (traverse (fun _ n => n) k x) at 2.
     - apply traverse_ext; simpl; intros.
       now destruct (le_gt_dec k0 n).
     - apply traverse_ids.
+  Qed. *)
+
+  Global Instance instantiate_proper:
+    Proper (subst_equiv ==> eq ==> eq) instantiate.
+  Proof.
+    intros x y ? a b ?; subst.
+    apply H0.
   Qed.
-
-  Goal
-    forall y s,
-    (y, s) 0 = y.
-  Proof.
-    intros.
-    unfold instantiate; simpl.
-    rewrite traverse_var; simpl.
-    now rewrite lift_zero_e_equals_e.
-  Qed.
-
-  Goal
-    forall y s n,
-    (y, s) (S n) = s n.
-  Proof.
-    intros.
-    unfold instantiate; simpl.
-    do 2 rewrite traverse_var; simpl.
-    now rewrite lift_zero_e_equals_e.
-  Qed.
-
-  Goal
-    forall (i n: nat),
-    L i n = i + n.
-  Proof.
-    intros.
-    unfold instantiate; simpl.
-    rewrite traverse_var; simpl.
-    reflexivity.
-  Qed.
-
-  Goal
-    forall x,
-    I x = x.
-  Proof.
-    intros.
-    (* This is not obvious, but it follows by definition! *)
-    apply lift_zero_e_equals_e.
-  Qed.
-
-  Goal
-    forall s t x,
-    (s o t) x = t (s x).
-  Proof.
-    intros.
-    unfold instantiate.
-    rewrite traverse_fun; simpl.
-    apply traverse_ext; clear x; intros.
-    replace (k + 0 - 0) with k; try lia.
-    destruct s;
-    simpl;
-    destruct (le_gt_dec k n);
-    simpl;
-    auto;
-    rewrite traverse_var;
-    destruct t;
-    simpl;
-    destruct (le_gt_dec k n);
-    try lia; auto.
-  Qed.
-
-  Goal
-    subst_equiv 0 I.
-  Proof.
-    intros x.
-    unfold instantiate.
-    apply traverse_ext; clear x; intros.
-    simpl; reflexivity.
-  Qed.
-
-  Goal
-    subst_equiv (0, 1) I.
-  Proof.
-    intros x.
-    unfold instantiate; simpl.
-    apply traverse_ext; clear x; intros.
-    destruct (le_gt_dec k n).
-    - remember (n - k) as m.
-      destruct m.
-      + unfold __lift.
-        rewrite traverse_var; simpl.
-        f_equal; lia.
-      + unfold __lift.
-        rewrite traverse_var; simpl.
-        f_equal; lia.
-    - reflexivity.
-  Qed.
-
-  Goal
-    forall y s,
-    subst_equiv (1 o (y, s)) s.
-  Proof.
-    intros y s x.
-    unfold instantiate; simpl.
-    apply traverse_ext; clear x; intros.
-    destruct (le_gt_dec k n).
-    - rewrite traverse_var.
-      simplify decidable equality.
-      (* TODO: should the above simplify this match as well...? *)
-      remember (S n - k) as m.
-      destruct m.
-      + exfalso; lia.
-      + replace m with (n - k) by lia.
-        clear Heqm m.
-        (* This law is valid! *)
-        admit.
-    - destruct s; simpl.
-      + now simplify decidable equality.
-      + now simplify decidable equality.
-      + now simplify decidable equality.
-      + now simplify decidable equality.
-  Admitted.
-
-  Goal
-    forall s,
-    subst_equiv (I o s) s.
-  Proof.
-    admit.
-  Admitted.
-
-  Goal
-    forall s,
-    subst_equiv (s o I) s.
-  Proof.
-    admit.
-  Admitted.
-
-  Goal
-    forall s t u,
-    subst_equiv (s o (t o u)) (s o t o u).
-  Proof.
-    admit.
-  Admitted.
-
-  Goal
-    forall y s t,
-    subst_equiv ((y, s) o t) (t y, s o t).
-  Proof.
-    admit.
-  Admitted.
-
-  Goal
-    (* This rule is useful for rewriting as that we can deal with metavariables
-       in the proof. *)
-    forall (s: substitution),
-    subst_equiv (s 0, 1 o s) s.
-  Proof.
-    admit.
-  Admitted.
 
 End DeBruijn.
 
-(* Global Opaque instantiate. *)
-
+Arguments substitution {X}.
 Arguments subst_id {X}.
 Arguments subst_lift {X}.
 Arguments subst_cons {X}.
 Arguments subst_comp {X}.
-
-(*
-  Global Hint Rewrite traverse_subst_instantiate: subst.
-  Global Hint Rewrite instantiate_cons_zero: subst.
-  Global Hint Rewrite instantiate_cons_succ: subst.
-  Global Hint Rewrite instantiate_lift: subst.
-  Global Hint Rewrite instantiate_id: subst.
-  Global Hint Rewrite instantiate_comp: subst.
-  Global Hint Rewrite subst_cons_lift_simpl: subst.
-  Global Hint Rewrite subst_lift_cons_simpl: subst.
-  Global Hint Rewrite subst_comp_left_identity: subst.
-  Global Hint Rewrite subst_comp_right_identity: subst.
-  Global Hint Rewrite subst_comp_assoc: subst.
-  Global Hint Rewrite subst_comp_cons_distr: subst.
-  Global Hint Rewrite subst_cons_eta: subst.
-*)
-
-(* -------------------------------------------------------------------------- *)
-
-(* Below we have some tests. TODO: remove this and move into proper places. *)
-
-Require Import Local.Prelude.
-Require Import Local.AbstractRewriting.
-Require Import Local.Syntax.
-Require Import Local.Metatheory.
-Require Import Local.Machine.
-
-Global Instance pseudoterm_deBruijn: deBruijn pseudoterm.
-Proof.
-  apply (Build_deBruijn bound traverse); intros.
-  - simpl.
-    reflexivity.
-  - generalize dependent k.
-    induction x using pseudoterm_deepind; simpl; auto; intros.
-    + f_equal.
-      list induction over H.
-    + f_equal; auto.
-      list induction over H.
-    + f_equal; auto.
-      list induction over H.
-  - generalize dependent k.
-    induction x using pseudoterm_deepind; simpl; auto; intros.
-    + f_equal.
-      (* TODO: make list induction rewrite a few things as well... *)
-      list induction over H0.
-      now do 2 rewrite traverse_list_length.
-    + f_equal; auto.
-      list induction over H0.
-    + f_equal; auto.
-      list induction over H0.
-      now do 2 rewrite traverse_list_length.
-  - generalize dependent j.
-    generalize dependent k.
-    induction x using pseudoterm_deepind; simpl; auto; intros.
-    + f_equal; lia.
-    + f_equal.
-      list induction over H.
-      do 3 rewrite traverse_list_length.
-      rewrite H.
-      admit.
-    + f_equal; auto.
-      list induction over H.
-    + f_equal; auto.
-      * admit.
-      * admit.
-      * admit.
-Admitted.
+Arguments subst_upn {X} {H}.
+Arguments subst_app {X}.
