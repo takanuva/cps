f59154e1dd8b67d15f03257d34a984b6771f54ec
Paulo Torrens <paulotorrens@gnu.org>
2023-12-21 23:38:01 +0000

    Start working with substitution algebra

--- theories/Substitution.v
+++ theories/Substitution.v
@@ -11,10 +11,6 @@ Require Import Morphisms.
 Import ListNotations.
 Set Implicit Arguments.
 
-(* *)
-
-Create HintDb sigma.
-
 (* TODO: move this tactic! Also... TODO: use this tactic more! *)
 
 Tactic Notation "simplify" "decidable" "equality" :=
@@ -31,6 +27,18 @@ Tactic Notation "simplify" "decidable" "equality" :=
 Section DeBruijn.
 
   Variable X: Set.
+
+  (* *)
+
+  Inductive substitution: Set :=
+    | subst_ids
+    | subst_lift (i: nat)
+    | subst_cons (y: X) (s: substitution)
+    | subst_comp (s: substitution) (t: substitution)
+    | subst_upn (i: nat) (s: substitution).
+
+  Definition subst_app (xs: list X) (s: substitution): substitution :=
+    fold_right subst_cons s xs.
 
   Class deBruijn: Type := {
     var:
@@ -54,134 +62,181 @@ Section DeBruijn.
         traverse (fun i n => traverse f (i + k - j) (g i n)) j x *)
   }.
 
-  Context `{deBruijn}.
+  Context `{dB: deBruijn}.
 
-  #[nonuniform]
-  Local Coercion var: nat >-> X.
-
-  (* TODO: we may want to mess around. We actually only need lift and cons in
-     here... id and comp are derivable. We could move those out, then we'd get
-     something similar to the primitives de Bruijn used. Or we could move up
-     into here to have something similar to the confluent sigma calculus... we
-     could also move app in here, as it's useful for parallel substitution. So
-     this will be a problem for future me (I'm so sorry). *)
-
-  Inductive substitution: Type :=
-    | subst_id
-    | subst_lift (i: nat)
-    | subst_cons (y: X) (s: substitution)
-    | subst_comp (s: substitution) (t: substitution).
-
-  Fixpoint subst_upn (i: nat) (s: substitution): substitution :=
-    match i with
-    | 0 =>
-      s
-    | S i =>
-      subst_cons 0 (subst_comp (subst_upn i s) (subst_lift 1))
-    end.
-
-  Definition subst_app (xs: list X) (s: substitution): substitution :=
-    fold_right subst_cons s xs.
+  Definition lift_fun i k n :=
+    if le_gt_dec k n then
+      var (i + n)
+    else
+      var n.
 
   Definition lift (i: nat): nat -> X -> X :=
-    traverse (fun k n =>
-      if le_gt_dec k n then
-        var (i + n)
-      else
-        var n).
+    traverse (lift_fun i).
 
-  (* Goal
-    forall i j k l x,
-    k <= l + j ->
-    l <= k ->
-    lift i k (lift j l x) = lift (i + j) l x.
-  Proof.
-    intros; unfold lift.
-    rewrite traverse_fun.
-    apply traverse_ext; clear x; intros.
-    rename k0 into p.
-    destruct (le_gt_dec p n); simpl.
-    - rewrite traverse_var.
-      simplify decidable equality.
-      f_equal; lia.
-    - rewrite traverse_var.
-      simplify decidable equality.
-      reflexivity.
-  Qed. *)
-
-  Fixpoint apply_subst (s: substitution) (k: nat) (n: nat): X :=
+  Fixpoint inst_fun (s: substitution) (k: nat) (n: nat): X :=
     if le_gt_dec k n then
       match s with
-      | subst_id =>
+      | subst_ids =>
         var n
       | subst_lift i =>
         var (i + n)
       | subst_cons y s =>
-        let x := match n - k with
-                 | 0 => y
-                 | S m => apply_subst s 0 m
-                 end
-        in lift k 0 x
+        if Nat.eq_dec k n then
+          lift k 0 y
+        else
+          inst_fun s k (n - 1)
       | subst_comp s t =>
-        traverse (apply_subst t) k (apply_subst s k n)
+        traverse (inst_fun t) k (inst_fun s k n)
+      | subst_upn i s =>
+        inst_fun s (i + k) n
       end
     else
       var n.
 
-  Local Notation instantiate_rec s k :=
-    (traverse (apply_subst s) k).
+  Definition inst (s: substitution): nat -> X -> X :=
+    traverse (inst_fun s).
 
-  Definition instantiate (s: substitution): X -> X :=
-    instantiate_rec s 0.
-
-  Global Coercion instantiate: substitution >-> Funclass.
-
-  Definition test (x: X) (s: substitution): X :=
-    instantiate s x.
-
-  #[nonuniform]
-  Global Coercion test: X >-> Funclass.
+  Global Coercion inst: substitution >-> Funclass.
 
   Goal
-    forall i k x,
-    (* We have intensional equality between these. *)
-    lift i k x = instantiate_rec (subst_lift i) k x.
+    forall i,
+    lift i = inst (subst_lift i).
   Proof.
+    (* We have intensional equality between these! *)
     auto.
   Qed.
 
-  (* TODO: should we delcare this as a setoid...? *)
-
   Definition subst_equiv (s: substitution) (t: substitution): Prop :=
-    (* TODO: It should be a sufficient and necessary condition to check only for
-       vars in here, so this would simplify stuff. *)
-    forall x,
-    instantiate s x = instantiate t x.
+    forall x k,
+    inst s k x = inst t k x.
 
-  (* Lemma lift_zero_e_equals_e:
-    forall k x,
-    lift 0 k x = x.
+  Global Instance inst_proper:
+    Proper (subst_equiv ==> eq ==> eq ==> eq) inst.
   Proof.
-    intros; unfold lift.
-    replace x with (traverse (fun _ n => n) k x) at 2.
-    - apply traverse_ext; simpl; intros.
-      now destruct (le_gt_dec k0 n).
-    - apply traverse_ids.
-  Qed. *)
-
-  Global Instance instantiate_proper:
-    Proper (subst_equiv ==> eq ==> eq) instantiate.
-  Proof.
-    intros x y ? a b ?; subst.
-    apply H0.
+    intros s t ? k _ () x _ ().
+    apply H.
   Qed.
 
 End DeBruijn.
 
 Arguments substitution {X}.
-Arguments subst_id {X}.
+Arguments subst_ids {X}.
 Arguments subst_lift {X}.
 Arguments subst_cons {X}.
 Arguments subst_comp {X}.
-Arguments subst_upn {X} {H}.
+Arguments subst_upn {X}.
 Arguments subst_app {X}.
+
+Create HintDb sigma.
+
+Ltac sigma :=
+  idtac.
+
+(* -------------------------------------------------------------------------- *)
+
+Section Tests.
+
+  (* For now, we're not actually checking that the rewriting rules above are
+     confluent and complete (although we indeed want them to be), as we need to
+     expand them a bit from the definition of the sigma calculus. In order to
+     make up for that, lets just check that the rewriting rules tactic is enough
+     to validate the algebraic laws for both the sigma SP calculus, which was
+     used by the autosubst library, and for the confluent sigma calculus. For
+     reference, both calculi are described in the "Confluence Properties of Weak
+     and Strong Calculi" paper. *)
+
+  Variable X: Set.
+  Context `{db: deBruijn X}.
+
+  Implicit Types x y z: X.
+  Implicit Types s t u: @substitution X.
+
+  Local Notation "s ~ t" := (subst_equiv s t) (at level 70, no associativity).
+
+  (* Lets first check the laws for the sigma SP calculus... *)
+
+  Goal
+    forall x s,
+    (* 0[x, s] = x *)
+    subst_cons x s 0 (var 0) = x.
+  Proof.
+    admit.
+  Admitted.
+
+  Goal
+    forall x s n,
+    subst_cons x s 0 (var (1 + n)) = s 0 (var n).
+  Proof.
+    admit.
+  Admitted.
+
+  Goal
+    forall n,
+    subst_lift 1 0 (var n) = var (1 + n).
+  Proof.
+    admit.
+  Admitted.
+
+  Goal
+    forall x,
+    subst_ids 0 x = x.
+  Proof.
+    admit.
+  Admitted.
+
+  Goal
+    forall x s t,
+    t 0 (s 0 x) = subst_comp s t 0 x.
+  Proof.
+    admit.
+  Admitted.
+
+  Goal
+    subst_cons (var 0) (subst_lift 1) ~ subst_ids.
+  Proof.
+    admit.
+  Admitted.
+
+  Goal
+    forall x s,
+    subst_comp (subst_lift 1) (subst_cons x s) ~ s.
+  Proof.
+    admit.
+  Admitted.
+
+  Goal
+    forall s,
+    subst_comp subst_ids s ~ s.
+  Proof.
+    admit.
+  Admitted.
+
+  Goal
+    forall s,
+    subst_comp s subst_ids ~ s.
+  Proof.
+    admit.
+  Admitted.
+
+  Goal
+    forall s t u,
+    subst_comp (subst_comp s t) u ~ subst_comp s (subst_comp t u).
+  Proof.
+    admit.
+  Admitted.
+
+  Goal
+    forall x s t,
+    subst_comp (subst_cons x s) t ~ subst_cons (t 0 x) (subst_comp s t).
+  Proof.
+    admit.
+  Admitted.
+
+  Goal
+    forall s,
+    subst_cons (s 0 (var 0)) (subst_comp (subst_lift 1) s) ~ s.
+  Proof.
+    admit.
+  Admitted.
+
+End Tests.
