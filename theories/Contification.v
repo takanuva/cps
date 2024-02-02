af2f094b8ba9611605169a98133671ed5ffa9fea
Paulo Torrens <paulotorrens@gnu.org>
2024-02-02 15:32:39 +0000

    Add theorem to show that contification is sound

--- theories/Contification.v
+++ theories/Contification.v
@@ -4,9 +4,15 @@
 
 Require Import Relations.
 Require Import Local.Prelude.
+Require Import Local.AbstractRewriting.
 Require Import Local.Syntax.
+Require Import Local.Context.
+Require Import Local.Metatheory.
+Require Import Local.Equational.
 (* TODO: I've defined not_free_context in the wrong file so fix this, please? *)
 Require Import Local.Reduction.
+Require Import Local.Machine.
+Require Import Local.Observational.
 
 (*
   The contification transformation, as presented by Kennedy:
@@ -25,4 +31,58 @@ Definition CONTI (R: relation pseudoterm): Prop :=
   forall h n (ts us: list pseudoterm) (b c: pseudoterm),
   not_free_context 0 h ->
   drop n ts us ->
-  False.
+  (* Of course this definition is still wrong. *)
+  R void void.
+
+Inductive cont: relation pseudoterm :=
+  | cont_conti:
+    CONTI cont
+  | cont_bind_left:
+    LEFT cont
+  | cont_bind_right:
+    RIGHT cont.
+
+Lemma sema_cont:
+  inclusion cont sema.
+Proof.
+  induction 1.
+  - admit.
+  - now apply sema_bind_left.
+  - now apply sema_bind_right.
+Admitted.
+
+Lemma barb_cont:
+  inclusion cont barb.
+Proof.
+  intros b c ?.
+  apply barb_sema.
+  apply sema_cont.
+  assumption.
+Qed.
+
+Theorem contification_is_sound:
+  forall b c,
+  cont b c ->
+  forall h: context,
+  big (h b, []) <-> big (h c, []).
+Proof.
+  intros.
+  apply barb_cont in H.
+  assert [h b ~~ h c].
+  - (* TODO: should we turn this into a lemma? *)
+    induction h; simpl.
+    + assumption.
+    + now apply barb_bind_left.
+    + now apply barb_bind_right.
+  - split; intros.
+    + apply big_implies_head_evaluation in H1 as (k, ?).
+      apply weak_convergence_characterization in H1.
+      apply head_evaluation_implies_big with k.
+      apply weak_convergence_characterization.
+      now apply barb_weak_convergence with (h b).
+    + apply big_implies_head_evaluation in H1 as (k, ?).
+      apply weak_convergence_characterization in H1.
+      apply head_evaluation_implies_big with k.
+      apply weak_convergence_characterization.
+      now apply barb_weak_convergence with (h c).
+Qed.
