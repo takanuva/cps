4ba40233566a3efce7b34a8c14a94680474e78ab
Paulo Torrens <paulotorrens@gnu.org>
2024-11-26 22:36:54 +0000

    Define call-by-value evaluation contexts

--- theories/Constructions/Reduction.v
+++ theories/Constructions/Reduction.v
@@ -29,3 +29,26 @@ Proof.
       apply n.
       now exists y.
 Qed.
+
+(* Call-by-value evaluation contexts. *)
+Inductive evaluation_context: context -> Prop :=
+  | evaluation_context_hole:
+    evaluation_context context_hole
+  | evaluation_context_app_left:
+    forall e f,
+    evaluation_context e ->
+    evaluation_context (context_app_left e f)
+  | evaluation_context_app_right:
+    forall v e,
+    value v ->
+    evaluation_context e ->
+    evaluation_context (context_app_right v e)
+  | evaluation_context_def_val:
+    forall e t f,
+    evaluation_context e ->
+    evaluation_context (context_def_val e t f)
+  | evaluation_context_def_body:
+    forall v t f,
+    value v ->
+    evaluation_context f ->
+    evaluation_context (context_def_body v t f).
