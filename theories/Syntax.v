ff0bbde027df877b127d57731ee48e283f01321b
Paulo Torrens <paulotorrens@gnu.org>
2025-02-11 22:32:17 +0000

    Separate types from pseudoterms (WIP)

--- theories/Syntax.v
+++ theories/Syntax.v
@@ -1,5 +1,5 @@
 (******************************************************************************)
-(*   Copyright (c) 2019--2023 - Paulo Torrens <paulotorrens AT gnu DOT org>   *)
+(*   Copyright (c) 2019--2025 - Paulo Torrens <paulotorrens AT gnu DOT org>   *)
 (******************************************************************************)
 
 Require Import List.
@@ -8,29 +8,30 @@ Require Import Local.Prelude.
 Require Import Local.AbstractRewriting.
 Require Import Local.Substitution.
 
-Export ListNotations.
+Import ListNotations.
 
 (** ** Syntax
 
-    Inspired by the lambda cube, we use [type] and [prop] as our universes, and
-    we keep [base] as our only base type. We also use [void] as the type of
-    commands, though it won't appear on any actual terms. As standard, we use de
-    Bruijn indexes on the [bound] constructor for variables. Types are simple;
-    our only type constructor is [negation], a polyadic type which represents
-    the negation of an N-tuple of types.
+    In the original formulation for this code, such as presented in the ICFP 24
+    paper, we had types directly encoded in the syntax for terms. In order to
+    properly reuse the proofs for different type systems (such as simple types
+    and dependent types), we now use a distinct definition. *)
 
-    The commands in our language are either a [jump], written as k<x, ...>, or a
-    [bind], written as b { k<x: t, ...> = c }. *)
-
-Inductive pseudoterm: Set :=
-  | type
-  | prop
+Inductive pseudotype: Set :=
   | base
-  | void
+  | negation (ts: list pseudotype)
+
+(** We give a higher order definition for the syntax of the CPS-calculus, in a
+    way inspired by how dependent type theories are usually given. This was
+    originally done so because we wanted to simplify the de Bruijn arithmetic
+    stuff. However, now that we got the sigma library doing most of the work,
+    there's no reason for this anymore and this code can shift to a first order
+    presentation. TODO: define atomics and do it. *)
+
+with pseudoterm: Set :=
   | bound (n: nat)
-  | negation (ts: list pseudoterm)
   | jump (f: pseudoterm) (xs: list pseudoterm)
-  | bind (b: pseudoterm) (ts: list pseudoterm) (c: pseudoterm).
+  | bind (b: pseudoterm) (ts: list pseudotype) (c: pseudoterm).
 
 Coercion bound: nat >-> pseudoterm.
 
@@ -58,83 +59,83 @@ Example ex1: pseudoterm :=
     [base; negation [base; base]; base]
       (jump 1 [bound 3; bound 0])).
 
+(** We define a predicate stating that a pseudoterm appears inside a pseudotype,
+    as in the dependent cases, to simplify the inductive hypotheses. *)
+
+Inductive dependent: pseudoterm -> pseudotype -> Prop :=
+  (* For now, we don't define any dependent types, so this is empty. *).
+
+Definition Dep (P: pseudoterm -> Prop) (t: pseudotype): Prop :=
+  forall b, dependent b t -> P b.
+
 (** As we have lists inside our pseudoterms, we'll need a stronger induction
     principle for it, stating that propositions are kept inside those lists. *)
 
 Definition pseudoterm_deepind:
   forall P: pseudoterm -> Prop,
-  forall f1: P type,
-  forall f2: P prop,
-  forall f3: P base,
-  forall f4: P void,
-  forall f5: (forall n, P (bound n)),
-  forall f6: (forall ts, Forall P ts -> P (negation ts)),
-  forall f7: (forall f xs, P f -> Forall P xs -> P (jump f xs)),
-  forall f8: (forall b ts c, P b -> Forall P ts -> P c -> P (bind b ts c)),
-  forall e, P e.
+  forall f1:
+    (forall n, P (bound n)),
+  forall f2:
+    (forall k xs, P k -> Forall P xs -> P (jump k xs)),
+  forall f3:
+    (forall b ts c, P b -> Forall (Dep P) ts -> P c -> P (bind b ts c)),
+  forall e,
+  P e.
 Proof.
-  do 9 intro; fix H 1.
+  do 4 intro; fix H 1.
   destruct e.
-  (* Case: type. *)
-  - apply f1.
-  (* Case: prop. *)
-  - apply f2.
-  (* Case: base. *)
-  - apply f3.
-  (* Case: void. *)
-  - apply f4.
   (* Case: bound. *)
-  - apply f5.
-  (* Case: negation. *)
-  - apply f6.
-    induction ts; auto.
+  - apply f1.
   (* Case: jump. *)
-  - apply f7; auto.
+  - apply f2; auto.
     induction xs; auto.
   (* Case: bind. *)
-  - apply f8; auto.
-    induction ts; auto.
+  - apply f3; auto.
+    induction ts.
+    + constructor.
+    + constructor; auto.
+      unfold Dep; intros.
+      inversion H0.
 Defined.
 
-(** Equality on pseudoterms is decidable. *)
+(** Equality on pseudoterms (and pseudotypes) is decidable. *)
 
 Lemma pseudoterm_eq_dec:
-  forall a b: pseudoterm,
-  { a = b } + { a <> b }.
-Proof.
-  fix H 1.
-  destruct a; destruct b; try (right; intro; discriminate).
-  (* Case: type. *)
-  - left; reflexivity.
-  (* Case: prop. *)
-  - left; reflexivity.
-  (* Case: base. *)
-  - left; reflexivity.
-  (* Case: void. *)
-  - left; reflexivity.
-  (* Case: bound. *)
-  - destruct Nat.eq_dec with n n0.
-    + left; congruence.
-    + right; congruence.
-  (* Case negation. *)
-  - destruct list_eq_dec with pseudoterm ts ts0.
-    + exact H.
-    + left; congruence.
-    + right; congruence.
-  (* Case: jump. *)
-  - destruct list_eq_dec with pseudoterm xs xs0.
-    + exact H.
-    + destruct H with a b; try (right; congruence).
+  forall b c: pseudoterm,
+  { b = c } + { b <> c }
+with pseudotype_eq_dec:
+  forall t u: pseudotype,
+  { t = u } + { t <> u }.
+Proof with try now (right; congruence).
+  - destruct b; destruct c...
+    (* Case: bound. *)
+    + rename n into n1, n0 into n2.
+      destruct Nat.eq_dec with n1 n2...
       left; congruence.
-    + right; congruence.
-  (* Case: bind. *)
-  - destruct list_eq_dec with pseudoterm ts ts0.
-    + exact H.
-    + destruct H with a1 b1; try (right; congruence).
-      destruct H with a2 b2; try (right; congruence).
-      left; congruence.
-    + right; congruence.
+    (* Case: jump. *)
+    + rename xs into xs1, xs0 into xs2.
+      destruct list_eq_dec with pseudoterm xs1 xs2...
+      * exact pseudoterm_eq_dec.
+      * destruct pseudoterm_eq_dec with b c...
+        left; congruence.
+    (* Case: bind. *)
+    + rename ts into ts1, ts0 into ts2.
+      destruct list_eq_dec with pseudotype ts1 ts2...
+      * exact pseudotype_eq_dec.
+      * destruct pseudoterm_eq_dec with b1 c1...
+        destruct pseudoterm_eq_dec with b2 c2...
+        left; congruence.
+  - destruct t; destruct u...
+    (* Case: base. *)
+    + now left.
+    (* Case: negation. *)
+    + rename ts into ts1, ts0 into ts2.
+      destruct list_eq_dec with pseudotype ts1 ts2...
+      * exact pseudotype_eq_dec.
+      * left; congruence.
 Qed.
+
+(* TODO: is this definition still needed...? *)
 
 Definition traverse_list f k: list pseudoterm -> list pseudoterm :=
   fold_right (fun t ts =>
@@ -142,22 +143,13 @@ Definition traverse_list f k: list pseudoterm -> list pseudoterm :=
 
 Fixpoint traverse f k e: pseudoterm :=
   match e with
-  | type =>
-    type
-  | prop =>
-    prop
-  | base =>
-    base
-  | void =>
-    void
   | bound n =>
     f k n
-  | negation ts =>
-    negation (traverse_list (traverse f) k ts)
   | jump x xs =>
     jump (traverse f k x) (map (traverse f k) xs)
   | bind b ts c =>
-    bind (traverse f (S k) b) (traverse_list (traverse f) k ts)
+    (* TODO: for now, types don't contain variables... *)
+    bind (traverse f (S k) b) ((* traverse_list (traverse f) k *) ts)
       (traverse f (k + length ts) c)
   end.
 
@@ -176,13 +168,6 @@ Proof.
   auto.
 Qed.
 
-Lemma inst_distributes_over_negation:
-  forall s ts,
-  inst s (negation ts) = negation (bsmap s 0 ts).
-Proof.
-  auto.
-Qed.
-
 Lemma inst_distributes_over_jump:
   forall s x xs,
   inst s (jump x xs) = jump (s 0 x) (smap s 0 xs).
@@ -192,13 +177,12 @@ Qed.
 
 Lemma inst_distributes_over_bind:
   forall s b ts c,
-  inst s (bind b ts c) = bind (s 1 b) (bsmap s 0 ts) (s (length ts) c).
+  inst s (bind b ts c) = bind (s 1 b) ts (s (length ts) c).
 Proof.
   auto.
 Qed.
 
 Global Hint Rewrite bound_var_equality_stuff using sigma_solver: sigma.
-Global Hint Rewrite inst_distributes_over_negation using sigma_solver: sigma.
 Global Hint Rewrite inst_distributes_over_jump using sigma_solver: sigma.
 Global Hint Rewrite inst_distributes_over_bind using sigma_solver: sigma.
 
@@ -235,25 +219,9 @@ Definition remove_binding k e: pseudoterm :=
   subst (bound 0) k e.
 
 Inductive not_free: nat -> pseudoterm -> Prop :=
-  | not_free_type:
-    forall n,
-    not_free n type
-  | not_free_prop:
-    forall n,
-    not_free n prop
-  | not_free_base:
-    forall n,
-    not_free n base
-  | not_free_void:
-    forall n,
-    not_free n void
   | not_free_bound:
     forall n m,
     n <> m -> not_free n m
-  | not_free_negation:
-    forall n ts,
-    not_free_list n ts ->
-    not_free n (negation ts)
   | not_free_jump:
     forall n x ts,
     not_free n x ->
@@ -261,10 +229,12 @@ Inductive not_free: nat -> pseudoterm -> Prop :=
   | not_free_bind:
     forall n b ts c,
     not_free (S n) b ->
-    not_free_list n ts ->
+    (* TODO: types don't have variables... yet. *)
+    (* not_free_list n ts -> *)
     not_free (length ts + n) c ->
     not_free n (bind b ts c)
 
+(* Checks a list in a telescope-like fashion. *)
 with not_free_list: nat -> list pseudoterm -> Prop :=
   | not_free_list_nil:
     forall n,
