93e17d66005377ffa5af06f45c45501fb450e4d6
Paulo Torrens <paulotorrens@gnu.org>
2025-02-27 18:03:30 +0000

    Ooops, fix issue with decidability of normal forms

--- theories/Constructions/Reduction.v
+++ theories/Constructions/Reduction.v
@@ -15,6 +15,7 @@ Require Import Local.Constructions.Normalization.
 Import ListNotations.
 
 (* For typeable terms, the normal form is computable. *)
+
 Lemma normal_form_is_decidable:
   forall g e t,
   typing g e t typed_conv ->
@@ -24,15 +25,14 @@ Proof.
   intros.
   apply strong_normalization in H.
   induction H using SN_ind.
-  destruct step_is_decidable with x g as [ (y, ?) | ? ].
+  destruct step_is_decidable with g x as [ (y, ?H) | ?H ].
   - destruct H2 with y as (z, ?, ?).
     + now apply t_step.
     + exists z; eauto with cps.
   - exists x.
     + apply rt_refl.
     + intros y ?.
-      apply n.
-      now exists y.
+      now apply H0 with y.
 Qed.
 
 (*
