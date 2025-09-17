24236d6fae7cdfa52ba52949b9270fe7e766014b
Paulo Torrens <paulotorrens@gnu.org>
2025-09-17 18:58:11 +0100

    Finish proofs: both of Plotkin's translations are intuitionistic

--- theories/Intuitionistic.v
+++ theories/Intuitionistic.v
@@ -40,7 +40,7 @@ Inductive lift_var (P: nat -> Prop): pseudoterm -> Prop :=
 Inductive intuitionistic: list polarity -> pseudoterm -> Prop :=
   (*
      k in D, xs in G
-    ----------------- (RET)
+    -----------------
       G; D |- k<xs>
   *)
   | intuitionistic_ret:
@@ -50,7 +50,7 @@ Inductive intuitionistic: list polarity -> pseudoterm -> Prop :=
     intuitionistic g (jump k xs)
   (*
      k in D, x, ys in G
-    -------------------- (APP)
+    --------------------
       G; D |- x<ys, k>
   *)
   | intuitionistic_app:
@@ -61,7 +61,7 @@ Inductive intuitionistic: list polarity -> pseudoterm -> Prop :=
     intuitionistic g (jump x (bound k :: ys))
   (*
      G; D, k |- b     G, ys; D |- c
-    -------------------------------- (CON)
+    --------------------------------
         G; D |- b { k<ys> = c }
   *)
   | intuitionistic_con:
@@ -70,8 +70,8 @@ Inductive intuitionistic: list polarity -> pseudoterm -> Prop :=
     intuitionistic (repeat cartesian (length ts) ++ g) c ->
     intuitionistic g (bind b ts c)
   (*
-     G, x; D |- b     G, ys; k 
-    ----------------------------------------------- (FUN)
+     G, x; D |- b     G, ys; k
+    ----------------------------
      G; D |- b { x<ys, k> = c }
   *)
   | intuitionistic_fun:
@@ -108,6 +108,92 @@ Qed.
 
 Require Import Local.Lambda.PlotkinCBV.
 
+Local Lemma technical1:
+  forall g t e,
+  (forall k, free k (abstraction t e) -> item cartesian (linear :: g) (S k)) ->
+  forall k,
+  free k (lift 1 1 e) ->
+  item cartesian (cartesian :: consumed :: consume g) k.
+Proof.
+  intros.
+  (* Notice that if k is zero then this is trivial. *)
+  destruct k.
+  - constructor.
+  - (* Now, k can't be zero as this contradicts H0. *)
+    destruct k.
+    + exfalso.
+      apply H0.
+      apply lifting_more_than_n_makes_not_free_n; auto.
+    + (* For the general case, we can proceed by our hypothesis. *)
+      do 2 constructor.
+      apply item_consume_cartesian_stable.
+      (* Proceed by steps, of course. *)
+      assert (free k (abstraction t e)).
+      * intro; apply H0; clear H0.
+        dependent destruction H1.
+        replace (S (S n)) with (n + 1 + 1) by lia.
+        apply not_free_lift.
+        now rewrite Nat.add_comm.
+      * apply H in H1.
+        now dependent destruction H1.
+Qed.
+
+Local Lemma technical2:
+  forall g f x,
+  (forall k, free k (application f x) -> item cartesian (linear :: g) (S k)) ->
+  forall k,
+  free k (lift 1 0 f) ->
+  item cartesian (linear :: g) k.
+Proof.
+  intros.
+  (* Clearly, k can't ever be zero. *)
+  destruct k.
+  - exfalso.
+    apply H0.
+    apply lifting_more_than_n_makes_not_free_n; auto.
+  - (* Proceed as the general case above. *)
+    constructor.
+    assert (free k (application f x)).
+    + intro; apply H0; clear H0.
+      dependent destruction H1.
+      replace (S n) with (n + 1 + 0) by lia.
+      apply not_free_lift.
+      now rewrite Nat.add_comm.
+    + apply H in H1.
+      now dependent destruction H1.
+Qed.
+
+Local Lemma technical3:
+  forall g f x,
+  (forall k, free k (application f x) -> item cartesian (linear :: g) (S k)) ->
+  forall k,
+  free k (lift 2 0 x) ->
+  forall p,
+  item cartesian (cartesian :: p :: g) k.
+Proof.
+  intros.
+  (* Similar to the above, k can't possibly be zero. *)
+  destruct k.
+  - exfalso.
+    apply H0.
+    apply lifting_more_than_n_makes_not_free_n; auto with arith.
+  - (* Yet again, k can't be zero. *)
+    destruct k.
+    + exfalso.
+      apply H0.
+      apply lifting_more_than_n_makes_not_free_n; auto.
+    + (* Now, proceed once more by the general case as above. *)
+      do 2 constructor.
+      assert (free k (application f x)).
+      * intro; apply H0; clear H0.
+        dependent destruction H1.
+        replace (S (S n)) with (n + 2 + 0) by lia.
+        apply not_free_lift.
+        now rewrite Nat.add_comm.
+      * apply H in H1.
+        now dependent destruction H1.
+Qed.
+
 Lemma plotkin_cbv_is_intuitionistic:
   forall e b,
   cbv_cps e b ->
@@ -139,7 +225,7 @@ Proof.
       * constructor.
       * dependent destruction H0.
         constructor; simpl.
-        admit.
+        now apply technical1 with t e.
   (* Case: application. *)
   - (* For CBV, we execute the left-hand side, then execute the right-hand
        side, then apply the function properly. Sure. *)
@@ -147,14 +233,14 @@ Proof.
     + apply IHcbv_cps1; auto with cps; intros.
       dependent destruction H1.
       constructor; simpl.
-      admit.
+      now apply technical2 with f x.
     + apply intuitionistic_con; simpl.
       * apply IHcbv_cps2; auto with cps; intros.
         dependent destruction H1.
         constructor; simpl.
-        admit.
+        now apply technical3 with f x.
       * now apply anchor_is_intuitionistic.
-Admitted.
+Qed.
 
 Theorem cbv_program_is_intuitionistic:
   forall e,
@@ -203,7 +289,7 @@ Proof.
       * constructor.
       * dependent destruction H0.
         constructor; simpl.
-        admit.
+        now apply technical1 with t e.
   (* Case: application. *)
   - (* Things a little bit different in here from CBV; we do one continuation,
        then we do a thunk declaration, and we apply the anchor. *)
@@ -211,7 +297,7 @@ Proof.
     + apply IHcbn_cps1; auto with cps; intros.
       dependent destruction H1.
       constructor; simpl.
-      admit.
+      now apply technical2 with f x.
     + (* Here the anchor is on the left. *)
       eapply intuitionistic_fun with (ts := []); simpl.
       * reflexivity.
@@ -219,8 +305,14 @@ Proof.
       * apply IHcbn_cps2; auto with cps; intros.
         dependent destruction H1.
         constructor; simpl.
-        admit.
-Admitted.
+        (* The third technicall lemma requires a tiny tweak in here too, since
+           now the thunk can't proceed with older continuations. *)
+        apply technical3 with f x; auto.
+        intros j ?; constructor.
+        apply item_consume_cartesian_stable.
+        specialize (H2 _ H1).
+        now dependent destruction H2.
+Qed.
 
 Theorem cbn_program_is_intuitionistic:
   forall e,
