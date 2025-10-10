eb46804c87c39adf6396214a6bc4ace14cc9c666
Paulo Torrens <paulotorrens@gnu.org>
2025-10-10 15:58:36 +0100

    Add file for CPS translation abstraction

---
+++ theories/Lambda/Translation.v
@@ -0,0 +1,7 @@
+(******************************************************************************)
+(*   Copyright (c) 2019--2025 - Paulo Torrens <paulotorrens AT gnu DOT org>   *)
+(******************************************************************************)
+
+(* TODO: the purpose of this file is to make a common (module) abstraction for
+   the CPS translations investigated. Ideally, they should contain proofs of
+   simulation and computational adequacy. *)
