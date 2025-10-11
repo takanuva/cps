d20a274d348f6b216b19356b183906940218694d
Paulo Torrens <paulotorrens@gnu.org>
2025-10-11 04:24:08 +0100

    Start sketching Kennedy's CPS translation

--- theories/Lambda/Kennedy.v
+++ theories/Lambda/Kennedy.v
@@ -2,11 +2,15 @@
 (*   Copyright (c) 2019--2025 - Paulo Torrens <paulotorrens AT gnu DOT org>   *)
 (******************************************************************************)
 
+Require Import List.
+Require Import Equality.
 Require Import Local.Prelude.
 Require Import Local.AbstractRewriting.
 Require Import Local.Substitution.
 Require Import Local.Lambda.Calculus.
 Require Import Local.Lambda.PlotkinCBV.
+
+Import ListNotations.
 
 (* The naïve one-pass CPS translation of Danvy and Filinski's that Kennedy
    describes in his paper is given as follows:
@@ -31,7 +35,9 @@ Require Import Local.Lambda.PlotkinCBV.
                    [e2] (\z2.
                     z1<z2, k>)) { k<v> = K(v) }
 
-   ...which, clearly, doesn't change the meaning of the term.
+   ...which, clearly, doesn't change the meaning of the term. We use Kennedy's
+   version merely as the continuation is kept closer, so it's easier to deal
+   with (and it remains as de Bruijn index zero).
 
    Kennedy provides an additional translation which we may also adapt, in that
    tail-calls are optimized on the fly. This may be done by modifying the above
@@ -56,3 +62,44 @@ Require Import Local.Lambda.PlotkinCBV.
    result that these translations are adequate and that they give a sound
    denotational semantics for the lambda calculus. Also, they may be helpful in
    studying ANF, as this translation should eliminate administrative redexes. *)
+
+Local Definition tail: Type :=
+  nat -> CPS.pseudoterm -> Prop.
+
+Local Definition halt: tail :=
+  fun n b =>
+    b = CPS.jump (CPS.bound 0) [CPS.bound (S n)].
+
+Inductive kennedy: term -> tail -> CPS.pseudoterm -> Prop :=
+  | kennedy_bound:
+    forall K n b,
+    K n b ->
+    kennedy (bound n) K b.
+
+(*
+     [-]: term -> (var -> pseudoterm) -> pseudoterm
+
+                            +------------+
+     [x] K = K(x)          \|/           |
+     [\x.e] K = K(f) { f<x, k> = [e] (\z.k<z>) }
+     [e1 e2] K = [e1] (\z1.
+                   [e2] (\z2.
+                    z1<z2, k> { k<v> = K(v) }))
+                                  |     /|\
+                                  +------+
+*)
+
+Goal
+  forall e b,
+  cbv_cps e b ->
+  forall c,
+  kennedy e halt c ->
+  Local.Reduction.conv b c.
+Proof.
+  (* We'll obviously have to strengthen the inductive hypothesis... *)
+  induction 1; intros.
+  - dependent destruction H.
+    apply Local.Reduction.conv_refl.
+  - admit.
+  - admit.
+Admitted.
