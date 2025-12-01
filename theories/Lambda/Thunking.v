32194e9ebb2ca67c117038d4deb7fee6af058340
Paulo Torrens <paulotorrens@gnu.org>
2025-12-01 14:50:45 +0000

    Add file for thunking translation

---
+++ theories/Lambda/Thunking.v
@@ -0,0 +1,12 @@
+(******************************************************************************)
+(*   Copyright (c) 2019--2025 - Paulo Torrens <paulotorrens AT gnu DOT org>   *)
+(******************************************************************************)
+
+Require Import Lia.
+Require Import List.
+Require Import Arith.
+Require Import Equality.
+Require Import Local.Prelude.
+Require Import Local.AbstractRewriting.
+Require Import Local.Substitution.
+Require Import Local.Lambda.Calculus.
