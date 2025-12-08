8c9e9943361376d66e6de6aeb32be41e4a60acd1
Paulo Torrens <paulotorrens@gnu.org>
2025-12-08 14:20:20 +0000

    Add notes on Murthy's translation's adequacy

--- theories/Lambda/MurthyCBN.v
+++ theories/Lambda/MurthyCBN.v
@@ -41,4 +41,26 @@ Require Import Local.Lambda.Calculus.
             in [M]
 
   Can we prove this is computationally adequate as well? Looks like fun!
+
+  For the translation of a beta-redex [(\x.M) N], we note that the abstraction
+  does not immediately return its result using the current continuation, but
+  rather, it defines the continuation for [M] by using it, and gives a thunk
+  value for x also using it. When put in place, the term will reduce to:
+
+    [(\x.M) N] = let x(k) = [N] in
+                 let k(v) = k(v) in   (jumps to outer k!)
+                 [M]
+
+  Oops! This looks close to the definition of a CBN let, but there's a nasty
+  eta-redex there. Although this doesn't require floating as the CBV one does,
+  it still prevents simple simulation by just jumps and garbage collection. We
+  would expect, just as in the CBV case, that extending the notion of shrinking
+  reduction will fix this. Yet, we may prove this by doing something similar to
+  what has been done in the CBV simulation.
+
+  We should be extra careful in verifying the properties for eta-redexes, tho.
+  It doesn't seem like they hold by the equational theory! How does this relate
+  to the observational theory? Is it a missing axiom that works in general, or
+  something that only works due to the image of the CPS translation...? TODO:
+  study this! And may the gods help me, I'm so tired...
 *)
