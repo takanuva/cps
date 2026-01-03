74c562a20853bdbe0ac4a64e84aeea91328e2d7e
Paulo Torrens <paulotorrens@gnu.org>
2026-01-03 19:43:33 +0000

    Add more examples of dependent types

--- theories/Dependent.v
+++ theories/Dependent.v
@@ -23,35 +23,112 @@ Local Coercion TT.bound: nat >-> TT.term.
 Local Coercion TT.sort: TT.universe >-> TT.term.
 Local Coercion TT.lift_judgement: TT.typing_judgement >-> Funclass.
 
+(* fun (T: Set) (U: T -> Set) (f: (Pi x: T.U x)) (y: T): U y => f y *)
+
+Example dependent_example1: TT.term :=
+  TT.abstraction TT.iset
+    (TT.abstraction (TT.pi 0 TT.iset)
+      (TT.abstraction (TT.pi 1 (TT.application 1 0))
+        (TT.abstraction 2
+          (TT.application 1 0)))).
+
+Example dependent_example1_type: TT.term :=
+  TT.pi TT.iset
+    (TT.pi (TT.pi 0 TT.iset)
+      (TT.pi (TT.pi 1 (TT.application 1 0))
+        (TT.pi 2
+          (TT.application 2 0)))).
+
+Example dependent_example1_typed:
+  TT.typing [] dependent_example1 dependent_example1_type TT.conv.
+Proof.
+  unfold dependent_example1.
+  unfold dependent_example1_type.
+  eapply TT.typing_abs.
+  eapply TT.typing_abs.
+  eapply TT.typing_abs.
+  eapply TT.typing_abs.
+  eapply TT.typing_app.
+  - eapply TT.typing_bound.
+    + repeat econstructor; vm_compute; reflexivity.
+    + repeat constructor.
+    + now simpl.
+    + now vm_compute.
+  - eapply TT.typing_bound.
+    + repeat econstructor; vm_compute; reflexivity.
+    + constructor.
+    + now simpl.
+    + now vm_compute.
+  - now vm_compute.
+Qed.
+
+(* fun (T: Set) (U: T -> Set) (x: T) (y: U x): Sigma z: T, U z => (x, y) *)
+
+Example dependent_example2: TT.term :=
+  TT.abstraction TT.iset
+    (TT.abstraction (TT.pi 0 TT.iset)
+      (TT.abstraction 1
+        (TT.abstraction (TT.application 1 0)
+          (TT.pair 1 0 (TT.sigma 3 (TT.application 3 0)))))).
+
+Example dependent_example2_type: TT.term :=
+  TT.pi TT.iset
+    (TT.pi (TT.pi 0 TT.iset)
+      (TT.pi 1
+        (TT.pi (TT.application 1 0)
+          (TT.sigma 3 (TT.application 3 0))))).
+
+Example dependent_example2_typed:
+  TT.typing [] dependent_example2 dependent_example2_type TT.conv.
+Proof.
+  unfold dependent_example2.
+  unfold dependent_example2_type.
+  eapply TT.typing_abs.
+  eapply TT.typing_abs.
+  eapply TT.typing_abs.
+  eapply TT.typing_abs.
+  eapply TT.typing_pair.
+  - eapply TT.typing_bound.
+    + repeat econstructor; vm_compute; reflexivity.
+    + repeat constructor.
+    + now simpl.
+    + now vm_compute.
+  - eapply TT.typing_bound.
+    + repeat econstructor; vm_compute; reflexivity.
+    + constructor.
+    + now simpl.
+    + now vm_compute.
+Qed.
+
 (* Eval cbv in let f: (forall T: Type, T -> T) :=
                  fun (T: Type) (x: T) =>
                    x
                in fun (x: f Set bool) =>
                  f bool x. *)
 
-Example dependent_example1a: TT.term :=
+Example dependent_example3a: TT.term :=
   TT.abstraction (TT.type 0) (TT.abstraction 0 0).
 
-Example dependent_example1b: TT.term :=
+Example dependent_example3b: TT.term :=
   TT.pi (TT.type 0) (TT.pi 0 1).
 
-Example dependent_example1c: TT.term :=
+Example dependent_example3c: TT.term :=
   TT.application (TT.application 0 TT.iset) TT.boolean.
 
-Example dependent_example1d: TT.term :=
+Example dependent_example3d: TT.term :=
   TT.application (TT.application 1 TT.boolean) 0.
 
-Example dependent_example1: TT.term :=
+Example dependent_example3: TT.term :=
   TT.definition
-    dependent_example1a
-    dependent_example1b
-    (TT.abstraction dependent_example1c dependent_example1d).
+    dependent_example3a
+    dependent_example3b
+    (TT.abstraction dependent_example3c dependent_example3d).
 
-Example dependent_example1_type: TT.term :=
+Example dependent_example3_type: TT.term :=
   TT.pi TT.boolean TT.boolean.
 
-Example dependent_example1_typed:
-  TT.typing [] dependent_example1 dependent_example1_type TT.conv.
+Example dependent_example3_typed:
+  TT.typing [] dependent_example3 dependent_example3_type TT.conv.
 Proof.
   (* This will need cumulativity and conversion. Oh boy! *)
   admit.
