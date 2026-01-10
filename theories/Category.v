18a8b8fe269cf530fb6b5f3ec1ad9d654c799137
Paulo Torrens <paulotorrens@gnu.org>
2026-01-10 22:25:01 +0000

    Add a bunch of documentation

--- theories/Category.v
+++ theories/Category.v
@@ -9,6 +9,18 @@ Require Import Morphisms.
 
 Set Primitive Projections.
 
+(* In general, category theory is agnostic in respect to the foundation used to
+   build it. As we are working within Coq's type theory, and our use case needs
+   a lot of reasoning on sets, we instead start by defining setoids. We will of
+   course take advantage a lot of Coq's generalized rewriting mechanisms. The
+   idea is to simulate quotient types, which are not available, constructively.
+
+   A setoid is just a carrier type equipped with an equivalence relation that
+   represents the desired notion of equality within this type. We define here
+   carrier as a reversible coercion (so that we may identify the carrier set
+   with the setoid itself), and setoid_equiv as an instance proving that the
+   packed relation is indeed an equivalence relation over the carrier type. *)
+
 Polymorphic Structure Setoid: Type := {
   carrier :> Type;
   equiv: relation carrier;
@@ -25,6 +37,13 @@ Notation "x == y" := (equiv x y)
 Notation "x =/= y" := (complement equiv x y)
   (at level 70, no associativity): type_scope.
 
+(* We define a notion of setoid morphisms (a structure-preserving function over
+   two setoids) as a type-theoretic function over the two carrier sets, along
+   with a proof that this is a proper morphism, preserving the structure. I.e.,
+   for setoids S and T, if x: S and y: S such that x == y, a morphism f: S ~> T
+   will guarantee that f x == f y. Notice the coercion for the packed function,
+   which is given for convenience. *)
+
 Polymorphic Structure SetoidMorphism (S: Setoid) (T: Setoid): Type := {
   setoid_fun: S -> T;
   setoid_fun_respectful:
@@ -36,6 +55,15 @@ Global Coercion setoid_fun: SetoidMorphism >-> Funclass.
 Global Existing Instance setoid_fun_respectful.
 
 Infix "~>" := SetoidMorphism (at level 90, right associativity).
+
+(* We take an almost standard definition for categories, by giving the desired
+   structure over (1) a type of objects, and (2) a family of setoids for sorting
+   morphisms. This definition also uses a postcomposition operator instead of
+   the more usual regular composition operator, though of course both are easily
+   equivalent (for every f and g, post f g = comp g f).
+
+   For convenience, we identify the category with the type of objects and the
+   family of morphisms. *)
 
 Polymorphic Structure Category: Type := {
   obj :> Type;
@@ -70,9 +98,20 @@ Arguments post_id_left {c x y}.
 Arguments post_id_right {c x y}.
 Arguments post_assoc {c x y z w}.
 
-Definition SmallCategory: Type := Category@{Set Set}.
+(* We use universe polymorphism in order to more easily tackle size issues. But
+   for convenience, we also define a notion of small category, which is just a
+   category where both the type of objects and the (carrier type of the) setoid
+   are small (thus live in Set).
 
-Global Typeclasses Transparent SmallCategory.
+   TODO: can we force this definition to live in Type 0...? *)
+
+SubClass SmallCategory: Type := Category@{Set Set}.
+
+(* We define the opposite category construction, which takes a category C and
+   build a new category C^op (here opposite C) by taking as the type of objects
+   the same one from C, but inverting all the morphism; i.e., f is a morphism
+   from X to Y in C if and only if it is a morphism from Y to X in C^op. All the
+   laws in the structure are derivable. *)
 
 Polymorphic Program Definition opposite (C: Category): Category := {|
   obj := C;
@@ -99,25 +138,12 @@ Obligation 4 of opposite.
   apply post_assoc.
 Qed.
 
-Polymorphic Structure Functor (C: Category) (D: Category): Type := {
-  mapping :> C -> D;
-  fmap {x y}: C x y -> D (mapping x) (mapping y);
-  fmap_respectful {x y}:
-    Proper (equiv ==> equiv) (@fmap x y);
-  fmap_id {x}:
-    @fmap x x (@id C x) == (@id D (mapping x));
-  fmap_comp {x y z}:
-    forall f: C x y,
-    forall g: C y z,
-    fmap (post f g) == post (fmap f) (fmap g)
-}.
-
-Global Existing Instance fmap_respectful.
-
-Arguments mapping {C D} F: rename.
-Arguments fmap {C D} F {x y}: rename.
-
-(* -------------------------------------------------------------------------- *)
+(* We define, as a test and an example, a category of sets and functions (which
+   we will not be using in favor of setoids, of course). As morphisms, we define
+   the desired notion of equivalence for functions as extensional equality (so
+   that we would not need to take that as an axiom, since it's not derivable in
+   Coq's type theory anyway). First we define a canonical setoid for functions,
+   and then proceed to build the canonical category for sets. *)
 
 Polymorphic Definition funext_eq T U: relation (T -> U) :=
   fun f g => forall x, f x = g x.
@@ -136,7 +162,7 @@ Qed.
 
 Global Canonical Structure FunctionSetoid.
 
-Global Program Definition SetCategory: Category := {|
+Global Program Example SetCategory: Category := {|
   obj := Set;
   hom T U := T -> U;
   id T x := x;
@@ -164,6 +190,11 @@ Obligation 4 of SetCategory.
 Qed.
 
 Global Canonical Structure SetCategory.
+
+(* We now do a similar thing and define a category of setoids and setoid
+   morphisms as previously defined. So first make a canonical definition to set
+   the desired notion of equality of morphisms (which is similar to functional
+   extensionality!), and then proceed to build the canonical category. *)
 
 Global Polymorphic Program Definition MorphismSetoid S T: Setoid := {|
   carrier := S ~> T;
@@ -217,13 +248,46 @@ Obligation 6 of SetoidCategory.
   reflexivity.
 Qed.
 
-(* TODO: fix this warning. *)
+(* TODO: fix this warning! *)
+
 Global Canonical Structure SetoidCategory.
 
-(* -------------------------------------------------------------------------- *)
+(* As usual, we define functors from categories C and D as structure-preserving
+   morphisms. So we keep to functions: one for converting objects from C to D,
+   and one for converting morphisms from C to D, which respects the categorical
+   structure. *)
+
+Polymorphic Structure Functor (C: Category) (D: Category): Type := {
+  mapping :> C -> D;
+  fmap {x y}: C x y -> D (mapping x) (mapping y);
+  fmap_respectful {x y}:
+    Proper (equiv ==> equiv) (@fmap x y);
+  fmap_id {x}:
+    @fmap x x (@id C x) == (@id D (mapping x));
+  fmap_comp {x y z}:
+    forall f: C x y,
+    forall g: C y z,
+    fmap (post f g) == post (fmap f) (fmap g)
+}.
+
+Global Existing Instance fmap_respectful.
+
+Arguments mapping {C D} F: rename.
+Arguments fmap {C D} F {x y}: rename.
+
+(* There are some distinct but equivalent definitions for presheafs; we take,
+   perhaps, the most common one (or close enough to it): that a presheaf on C is
+   a functor from C^op to the category of setoids. The usual definition goes to
+   the category of sets instead, of course, so what we define is technically a
+   "setoid-valued presheaf in C". *)
 
 Polymorphic Definition Presheaf (C: Category): Type :=
   Functor (opposite C) Setoid.
+
+(* For convenience, we also treat presheafs as if defined by restricting maps;
+   a presheaf G in C will have, for every object X of C, a setoid G X, and also
+   a restriction operation: for every morphism C Y X, the presheaf will map from
+   G X into G Y, thus restricting the basic elements used within the set. *)
 
 Polymorphic Section Restriction.
 
