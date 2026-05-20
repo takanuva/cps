344bd4a98beb3064d19e4718cd8f13ace9517c39
Paulo Torrens <paulotorrens@gnu.org>
2026-05-20 19:25:12 +0100

    Some progress on universe construction

--- theories/Universe.v
+++ theories/Universe.v
@@ -8,6 +8,7 @@ Require Import Program.
 Require Import Equality.
 Require Import Local.InductionRecursion.
 Require Import Local.Setoid.
+Require Import Local.Category.
 
 Import ListNotations.
 Set Universe Polymorphism.
@@ -27,9 +28,13 @@ Set Primitive Projections.
 
 Section Family.
 
-  Variable A: SmallSetoid.
-  Variable B: A -> Setoid.
-  Variable C: list (forall (X: Setoid), (X -> Setoid) -> Setoid).
+  Monomorphic Universe u.
+
+  Local Notation TYPE := (Type@{u}).
+
+  Variable A: Set.
+  Variable B: A -> TYPE.
+  Variable C: list (forall (X: TYPE), (X -> TYPE) -> TYPE).
 
   Variant branch: Set :=
     | U_idx
@@ -37,7 +42,7 @@ Section Family.
     | U_ctor.
 
   Local Notation ctor_index :=
-    ({ n: nat | n < length C }).
+    { n: nat | n < length C }.
 
   Local Program Definition GET_CTOR (n: ctor_index) :=
     match nth_error C n with
@@ -51,18 +56,18 @@ Section Family.
     - auto.
   Qed.
 
-  Definition TARSKI: @Sig Setoid :=
+  Definition TARSKI: @Sig TYPE :=
     sigma branch (fun b: branch =>
       match b with
       | U_idx =>
-        iota (A: Setoid)
+        iota (A: TYPE)
       | U_lift =>
         sigma A (fun a: A =>
           iota (B a))
       | U_ctor =>
         sigma ctor_index (fun n =>
-          delta unit (fun Ta: unit -> Setoid =>
-            delta (Ta tt) (fun Tb: Ta tt -> Setoid =>
+          delta unit (fun Ta: unit -> TYPE =>
+            delta (Ta tt) (fun Tb: Ta tt -> TYPE =>
               iota (GET_CTOR n (Ta tt) Tb))))
       end).
 
@@ -74,7 +79,7 @@ Section Family.
   Local Definition IND: Type :=
     E TARSKI (total (muE TARSKI)) projT1.
 
-  Local Definition REC: IND -> Setoid :=
+  Local Definition REC: IND -> TYPE :=
     F TARSKI (total (muE TARSKI)) projT1.
 
   Local Definition embed (x: IND): total (muE TARSKI) :=
@@ -118,6 +123,16 @@ Section Family.
   Definition get_branch (c: CODE): branch :=
     let (x, ok) := c in projT1 x.
 
+  Definition get_inner:
+    forall c: CODE,
+    forall H: U_lift = get_branch c,
+    A.
+  Proof.
+    intros.
+    destruct c.
+    now destruct ok.
+  Defined.
+
   Definition left_child:
     forall c: CODE,
     U_ctor = get_branch c ->
@@ -134,5 +149,112 @@ Section Family.
       inversion H.
     - exact (shrink a ok).
   Defined.
+
+  Structure valid_type (c: CODE) (T: TYPE): Type := witness {
+    cast:
+      forall a H, c = shrink a H -> T -> REC a
+  }.
+
+  Arguments cast {c} {T} v {a} {H}.
+
+  Definition right_child:
+    forall c: CODE,
+    forall H: U_ctor = get_branch c,
+    forall T: TYPE,
+    forall X: valid_type (left_child c H) T,
+    forall x: T,
+    CODE.
+  Proof.
+    intros.
+    destruct c.
+    destruct ok; try easy.
+    simpl in *.
+    set (y := cast X eq_refl x).
+    exact (shrink (b y) (ok_b y)).
+  Defined.
+
+  Inductive subterm: CODE -> CODE -> Prop :=
+    | subterm_ctor_left:
+      forall c H,
+      subterm (left_child c H) c
+    | subterm_ctor_right:
+      forall c H T X x,
+      subterm (right_child c H T X x) c.
+
+  Lemma subterm_acc:
+    forall c: CODE,
+    Acc subterm c.
+  Proof.
+    intros.
+    destruct c.
+    induction ok.
+    - constructor; intros.
+      inversion H; easy.
+    - constructor; intros.
+      inversion H; easy.
+    - constructor; intros.
+      inversion_clear H0.
+      + simpl.
+        assumption.
+      + simpl in X |- *.
+        apply H.
+  Defined.
+
+  Lemma CODE_prim_rect:
+    forall P: CODE -> Type,
+    forall f1: P (shrink IDX' canonical_idx),
+    forall f2: (forall a: A, P (shrink (LIFT' a) (canonical_lift a))),
+    forall f3: (forall x: CODE,
+                forall H: U_ctor = get_branch x,
+                P (left_child x H) ->
+                (forall T X y, P (right_child x H T X y)) ->
+                P x),
+    forall x: CODE,
+    P x.
+  Proof.
+    intros.
+    assert (Acc subterm x) by now apply subterm_acc.
+    induction H; clear H.
+    remember (get_branch x) as b.
+    destruct b.
+    - enough (x = shrink IDX' canonical_idx).
+      + rewrite H.
+        apply f1.
+      + destruct x.
+        now destruct ok.
+    - set (a := get_inner x Heqb).
+      enough (x = shrink (LIFT' a) (canonical_lift a)).
+      + rewrite H.
+        apply f2.
+      + destruct x.
+        now destruct ok.
+    - apply f3 with Heqb.
+      + apply X.
+        constructor.
+      + intros.
+        apply X.
+        constructor.
+  Qed.
+
+  (*
+
+  Local Notation GOAL c :=
+    { x: IND & canonical x & forall a H, c = shrink a H -> x == a }.
+
+  Definition get_ind {c: CODE} (x: GOAL c): IND :=
+    let (a, ok_a, H) := x in a.
+
+  Definition get_canonical {c: CODE} (x: GOAL c): canonical (get_ind x) :=
+    let (a, ok_a, H) return canonical (get_ind x) := x in ok_a.
+
+  (* The main component: from any code, we can rebuild its elements, while
+     recursively also keeping track that the constructor is injective. *)
+
+  Definition rebuild:
+    forall x: CODE,
+    GOAL x.
+  Proof.
+
+  *)
 
 End Family.
