556537dab275f7c29b2f5f00cf10aa5658b3d6c5
Paulo Torrens <paulotorrens@gnu.org>
2026-05-25 13:30:18 +0100

    Reconstruct universes (with extensionality... for now?)

--- theories/Universe.v
+++ theories/Universe.v
@@ -6,40 +6,30 @@ Require Import Lia.
 Require Import List.
 Require Import Program.
 Require Import Equality.
+Require Import FunctionalExtensionality.
 Require Import Local.InductionRecursion.
 Require Import Local.Setoid.
 Require Import Local.Category.
 
+Import EqNotations.
 Import ListNotations.
 Set Universe Polymorphism.
 Set Primitive Projections.
 
-(* This file will have a version of my technique for encoding Tarski universes
-   in Coq, as shown in https://github.com/takanuva/tarski-rocq, but this time
-   using setoids to avoid the need for functional extensionality. Of course I
-   would prefer to avoid uniqueness of identity proofs as well, but most of my
-   codebase in this repository uses it already... so why bother? Sorry.
-
-   We also construct an enhanced version; instead of abstracting the universes
-   over some family of types, we also abstract them over types constructors, so
-   that we may also build Palmgren's notion of a superuniverse and we may have
-   codes for D-sets within the universes (for the proof of strong normalization
-   for the dependent type theory). *)
+(* ... *)
 
 Section Family.
 
-  Monomorphic Universe u.
+  Universe u0.
 
-  Local Notation TYPE := (Type@{u}).
+  Local Notation TYPE := (Type@{u0}).
+
+  Local Notation constructor :=
+    (forall X: TYPE, (X -> TYPE) -> TYPE).
 
   Variable A: Set.
   Variable B: A -> TYPE.
-  Variable C: list (forall (X: TYPE), (X -> TYPE) -> TYPE).
-
-  Variant branch: Set :=
-    | U_idx
-    | U_lift
-    | U_ctor.
+  Variable C: list constructor.
 
   Local Notation ctor_index :=
     { n: nat | n < length C }.
@@ -55,6 +45,11 @@ Section Family.
     - eassumption.
     - auto.
   Qed.
+
+  Variant branch: Set :=
+    | U_idx
+    | U_lift
+    | U_ctor.
 
   Definition TARSKI: @Sig TYPE :=
     sigma branch (fun b: branch =>
@@ -133,6 +128,19 @@ Section Family.
     now destruct ok.
   Defined.
 
+  Definition get_index:
+    forall c: CODE,
+    forall H: U_ctor = get_branch c,
+    ctor_index.
+  Proof.
+    intros.
+    destruct c.
+    now destruct ok.
+  Defined.
+
+  Definition valid_type (c: CODE) (T: TYPE): Type :=
+    forall a H, c = shrink a H -> T = REC a.
+
   Definition left_child:
     forall c: CODE,
     U_ctor = get_branch c ->
@@ -150,26 +158,18 @@ Section Family.
     - exact (shrink a ok).
   Defined.
 
-  Structure valid_type (c: CODE) (T: TYPE): Type := witness {
-    cast:
-      forall a H, c = shrink a H -> T -> REC a
-  }.
-
-  Arguments cast {c} {T} v {a} {H}.
-
   Definition right_child:
     forall c: CODE,
     forall H: U_ctor = get_branch c,
     forall T: TYPE,
     forall X: valid_type (left_child c H) T,
-    forall x: T,
-    CODE.
+    T -> CODE.
   Proof.
     intros.
     destruct c.
     destruct ok; try easy.
     simpl in *.
-    set (y := cast X eq_refl x).
+    set (y := rew dependent X a ok eq_refl in X0).
     exact (shrink (b y) (ok_b y)).
   Defined.
 
@@ -194,9 +194,9 @@ Section Family.
       inversion H; easy.
     - constructor; intros.
       inversion_clear H0.
-      + simpl.
+      + simpl in *.
         assumption.
-      + simpl in X |- *.
+      + simpl in *.
         apply H.
   Defined.
 
@@ -236,10 +236,8 @@ Section Family.
         constructor.
   Qed.
 
-  (*
-
   Local Notation GOAL c :=
-    { x: IND & canonical x & forall a H, c = shrink a H -> x == a }.
+    { x: IND & canonical x & valid_type c (REC x) }.
 
   Definition get_ind {c: CODE} (x: GOAL c): IND :=
     let (a, ok_a, H) := x in a.
@@ -247,14 +245,39 @@ Section Family.
   Definition get_canonical {c: CODE} (x: GOAL c): canonical (get_ind x) :=
     let (a, ok_a, H) return canonical (get_ind x) := x in ok_a.
 
-  (* The main component: from any code, we can rebuild its elements, while
-     recursively also keeping track that the constructor is injective. *)
-
   Definition rebuild:
     forall x: CODE,
     GOAL x.
   Proof.
-
-  *)
+    intros.
+    induction x using CODE_prim_rect.
+    - exists IDX'; repeat intro.
+      + constructor.
+      + apply (f_equal get_branch) in H0.
+        now destruct H.
+    - exists (LIFT' a); repeat intro.
+      + constructor.
+      + pose proof H0.
+        apply (f_equal get_branch) in H1; simpl in H1.
+        destruct H; try easy.
+        inversion H0; subst.
+        reflexivity.
+    - destruct IHx as (a, ok_a, ?H).
+      specialize (X (REC a) H0).
+      exists (CTOR' (get_index x H) a (fun y => get_ind (X y))); repeat intro.
+      + constructor; intros.
+        * assumption.
+        * apply get_canonical.
+      + subst.
+        destruct H1; try easy; simpl in *; clear H.
+        remember (H0 a0 H1 eq_refl) as f.
+        clear H0 Heqf; rename a0 into a'.
+        change (REC (CTOR' ?n ?a ?b)) with
+          (GET_CTOR n (REC a) (fun y => REC (b y))); simpl.
+        destruct f; f_equal.
+        extensionality y.
+        destruct (X y); simpl.
+        eapply v; reflexivity.
+  Qed.
 
 End Family.
