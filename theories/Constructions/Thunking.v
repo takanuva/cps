664ae1db5d83c92b3018fc3a8f6e4c22c24b4413
Paulo Torrens <paulotorrens@gnu.org>
2026-06-18 19:56:47 +0100

    Add file for checking thunking translation

---
+++ theories/Constructions/Thunking.v
@@ -0,0 +1,12 @@
+(******************************************************************************)
+(*   Copyright (c) 2019--2026 - Paulo Torrens <paulotorrens AT gnu DOT org>   *)
+(******************************************************************************)
+
+Require Import List.
+Require Import Relations.
+Require Import Equality.
+Require Import Local.Prelude.
+Require Import Local.Substitution.
+Require Import Local.AbstractRewriting.
+Require Import Local.Constructions.Calculus.
+Require Import Local.Constructions.TypeSystem.
