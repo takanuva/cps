2698f7b31b16b44f6ee592e1f916009a54684b07
Paulo Torrens <paulotorrens@gnu.org>
2026-09-14 21:50:23 +0100

    Add file for linear logic

---
+++ theories/Pi/LinearLogic.v
@@ -0,0 +1,14 @@
+(******************************************************************************)
+(*   Copyright (c) 2019--2026 - Paulo Torrens <paulotorrens AT gnu DOT org>   *)
+(******************************************************************************)
+
+Require Import Lia.
+Require Import Arith.
+Require Import Setoid.
+Require Import List.
+Require Import Relations.
+Require Import Equality.
+Require Import Local.Prelude.
+Require Import Local.AbstractRewriting.
+Require Import Local.Substitution.
+(* Require Import Local.Pi.Calculus. *)
